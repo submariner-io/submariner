a431915cce3073ee2dcd42d2f622beb0342f9fce
Janki Chhatbar <jchhatba@redhat.com>
2019-05-29 15:08:23 +0530

    Added some tests for route.go
    Signed-off-by: Janki Chhatbar <jchhatba@redhat.com>

--- pkg/routeagent/controllers/route/route_test.go
+++ pkg/routeagent/controllers/route/route_test.go
@@ -5,30 +5,41 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Route", func() {
-	Describe("Unit test for router.go", func() {
-		var rc RouteController
-		var testinputcidrblocks []string
+var _ = Describe("RouteController", func() {
 
-		BeforeEach(func() {
-			testinputcidrblocks = []string{"10.10.10.0/24", "192.168.1.0/24"}
+	Describe("Function populateCidrBlockList", func() {
+
+		Context("subnets does not contain CIDR blocks", func() {
+			It("should append CIDR block to subnets", func() {
+				routeController := RouteController{subnets: []string{"192.168.1.0/24"}}
+				routeController.populateCidrBlockList([]string{"10.10.10.0/24", "192.168.1.0/24"})
+				want := []string{"192.168.1.0/24", "10.10.10.0/24"}
+				Expect(routeController.subnets).To(Equal(want))
+			})
 		})
 
-		Context("Testing for function populateCidrBlockList", func() {
-			It("should append cidrblock to subnet if subnets doesnot contain cidrblock", func() {
-				rc.subnets = []string{"192.168.1.0/24"}
-				rc.populateCidrBlockList(testinputcidrblocks)
-				want := []string{"192.168.1.0/24", "10.10.10.0/24"}
-				Expect(rc.subnets).To(Equal(want))
-			})
-
-			It("Should not append if subnets contain cidrblock", func() {
-				rc.subnets = []string{"10.10.10.0/24", "192.168.1.0/24", "192.168.0.0./24"}
-				rc.populateCidrBlockList(testinputcidrblocks)
+		Context("subnets contain CIDR block", func() {
+			It("Should not append CIDR block subnets", func() {
+				routeController := RouteController{subnets: []string{"10.10.10.0/24", "192.168.1.0/24", "192.168.0.0./24"}}
+				routeController.populateCidrBlockList([]string{"10.10.10.0/24", "192.168.1.0/24"})
 				want := []string{"10.10.10.0/24", "192.168.1.0/24", "192.168.0.0./24"}
-				Expect(rc.subnets).To(Equal(want))
+				Expect(routeController.subnets).To(Equal(want))
 			})
 
+		})
+	})
+
+	Describe("Function containsString", func() {
+		Context("Given array of strings contain specified string", func() {
+			It("Should return true", func() {
+				Expect(containsString([]string{"unit", "test"}, "unit")).To(BeTrue())
+			})
+		})
+
+		Context("Given array of strings does not contain specified string", func() {
+			It("Should return false", func() {
+				Expect(containsString([]string{"unit", "test"}, "ginkgo")).To(BeFalse())
+			})
 		})
 	})
 
