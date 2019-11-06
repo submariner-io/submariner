92d4c83a46905d94f145c18e12b4c183262fb9fb
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2019-11-06 12:32:41 +0530

    Initial Submariner IPAM controller
    Initial poc commit for ipam controller. This
    will be refactored based on discussions and
    review comments.
    
    Do not merge this by itself.

---
+++ pkg/ipmanager/ipam/types.go
@@ -0,0 +1,31 @@
+package ipam
+
+import (
+	kubeInformers "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+)
+
+type SubmarinerIpamControllerSpecification struct {
+	ClusterID  string
+	GlobalCIDR string
+	ExcludeNS  []string
+}
+
+type InformerConfigStruct struct {
+	KubeClientSet   kubernetes.Interface
+	ServiceInformer kubeInformers.ServiceInformer
+	PodInformer     kubeInformers.PodInformer
+}
+
+type Controller struct {
+	kubeClientSet    kubernetes.Interface
+	serviceWorkqueue workqueue.RateLimitingInterface
+	servicesSynced   cache.InformerSynced
+	podWorkqueue     workqueue.RateLimitingInterface
+	podsSynced       cache.InformerSynced
+
+	excludeNamespaces map[string]bool
+	pool              *IpPool
+}
