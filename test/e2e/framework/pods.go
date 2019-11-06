725519544e19fec635be10da2e4cee8eb20000bb
Tom Pantelis <tompantelis@gmail.com>
2019-11-06 18:12:43 -0500

    E2E - modify CheckResultFunc to return status message
    CheckResultFunc now returns an optional string which is used by AwaitUntil
    to provide additional context when an error is returned from PollImmediate.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- test/e2e/framework/pods.go
+++ test/e2e/framework/pods.go
@@ -1,6 +1,8 @@
 package framework
 
 import (
+	"fmt"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -12,14 +14,13 @@ func (f *Framework) AwaitPodsByAppLabel(cluster ClusterIndex, appName string, na
 		return f.ClusterClients[cluster].CoreV1().Pods(namespace).List(metav1.ListOptions{
 			LabelSelector: "app=" + appName,
 		})
-	}, func(result interface{}) (bool, error) {
+	}, func(result interface{}) (bool, string, error) {
 		pods := result.(*v1.PodList)
 		if expectedCount < 0 || len(pods.Items) == expectedCount {
-			return true, nil
+			return true, "", nil
 		}
 
-		Logf("Actual pod count %d does not match the expected pod count %d - retrying...", len(pods.Items), expectedCount)
-		return false, nil
+		return false, fmt.Sprintf("Actual pod count %d does not match the expected pod count %d", len(pods.Items), expectedCount), nil
 	}).(*v1.PodList)
 }
 
