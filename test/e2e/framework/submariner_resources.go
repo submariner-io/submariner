2ffed495e842c42b81c8a2de18f30f43d97d6735
Tom Pantelis <tompantelis@gmail.com>
2019-11-09 08:17:56 -0500

    Add second gateway failover scenario
    Two gateway nodes configured with 1 submariner engine replica.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- test/e2e/framework/submariner_resources.go
+++ test/e2e/framework/submariner_resources.go
@@ -2,12 +2,48 @@ package framework
 
 import (
 	. "github.com/onsi/gomega"
+	submarinerv1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
 	submarinerClientset "github.com/submariner-io/submariner/pkg/client/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+type CheckEndpointFunc func(endpoint *submarinerv1.Endpoint) (bool, string, error)
 
 func (f *Framework) createSubmarinerClient(context string) *submarinerClientset.Clientset {
 	restConfig := f.createRestConfig(context)
 	clientSet, err := submarinerClientset.NewForConfig(restConfig)
 	Expect(err).NotTo(HaveOccurred())
 	return clientSet
+}
+
+func NoopCheckEndpoint(endpoint *submarinerv1.Endpoint) (bool, string, error) {
+	return true, "", nil
+}
+
+func (f *Framework) AwaitSubmarinerEndpoint(cluster ClusterIndex, checkEndpoint CheckEndpointFunc) *submarinerv1.Endpoint {
+	var retEndpoint *submarinerv1.Endpoint
+	AwaitUntil("find the submariner endpoint for "+TestContext.KubeContexts[cluster], func() (interface{}, error) {
+		return f.SubmarinerClients[cluster].SubmarinerV1().Endpoints(TestContext.SubmarinerNamespace).List(metav1.ListOptions{})
+	}, func(result interface{}) (bool, string, error) {
+		endpoints := result.(*submarinerv1.EndpointList)
+		retEndpoint = nil
+		for i := range endpoints.Items {
+			if endpoints.Items[i].Spec.ClusterID == TestContext.KubeContexts[cluster] {
+				if retEndpoint == nil {
+					retEndpoint = &endpoints.Items[i]
+				} else {
+					// We expect one Endpoint at a time per cluster
+					return false, "Multiple endpoints found", nil
+				}
+			}
+		}
+
+		if retEndpoint == nil {
+			return false, "No endpoint found", nil
+		}
+
+		return checkEndpoint(retEndpoint)
+	})
+
+	return retEndpoint
 }
