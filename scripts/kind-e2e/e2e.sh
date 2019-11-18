6a9f5de8f6693612cecc486cf3deca56ce7b6ebb
Vishal Thapar <5137689+vthapar@users.noreply.github.com>
2019-11-18 14:23:11 +0530

    Build and package globalnet
    1. Adds build, package and release scripts for globalnet.
    2. Adds globalnet to e2e scripts for E2E. Will be replaced with
       operator once operator changes are merged.

--- scripts/kind-e2e/e2e.sh
+++ scripts/kind-e2e/e2e.sh
@@ -195,6 +195,7 @@ function kind_import_images() {
     OPERATOR_IMAGE=${OPERATOR_IMAGE:-quay.io/submariner/submariner-operator:0.0.1}
     docker tag quay.io/submariner/submariner:dev submariner:local
     docker tag quay.io/submariner/submariner-route-agent:dev submariner-route-agent:local
+    docker tag quay.io/submariner/submariner-globalnet:dev submariner-globalnet:local
 
     if [[ "$deploy_operator" = true ]]; then
 
@@ -209,6 +210,7 @@ function kind_import_images() {
         echo "Loading submariner images in to cluster${i}..."
         kind --name cluster${i} load docker-image submariner:local
         kind --name cluster${i} load docker-image submariner-route-agent:local
+        kind --name cluster${i} load docker-image submariner-globalnet:local
         if [[ "$deploy_operator" = true ]]; then
              kind --name cluster${i} load docker-image submariner-operator:local
 	fi
@@ -321,6 +323,14 @@ function test_with_e2e_tests {
 	      -ginkgo.noColor -ginkgo.reportPassed \
         -ginkgo.reportFile ${DAPPER_SOURCE}/${DAPPER_OUTPUT}/e2e-junit.xml 2>&1 | \
         tee ${DAPPER_SOURCE}/${DAPPER_OUTPUT}/e2e-tests.log
+}
+
+function deploy_globalnet {
+    for i in 2 3; do
+      kubectl --context=cluster${i} -n $subm_ns create serviceaccount submariner-globalnet
+      kubectl --context=cluster${i} -n $subm_ns apply -f ${PRJ_ROOT}/scripts/kind-e2e/globalnet/role-globalnet.yaml
+      kubectl --context=cluster${i} -n $subm_ns apply -f ${PRJ_ROOT}/scripts/kind-e2e/globalnet/deploy-globalnet-${i}.yaml
+    done
 }
 
 function cleanup {
@@ -476,6 +486,7 @@ elif [[ $5 = helm ]]; then
     done
 fi
 
+deploy_globalnet
 test_connection
 test_with_e2e_tests
 
