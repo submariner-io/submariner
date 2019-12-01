9b7e2d808e40b21b84453cac655b2c0c50bbb881
Sridhar Gaddam <sgaddam@redhat.com>
2019-12-01 21:02:28 +0530

    Implement globalnet ingress and egress features
    This patch implements the following
    1. Spawns Globalnet Controller on the GatewayNode
    2. Initializes Globalnet Controller only when the Cluster uses kubeproxy
    3. Programs necessary egress rules for PODs
    4. Programs necessary ingress rules for ClusterIPServices
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- package/submariner-globalnet.sh
+++ package/submariner-globalnet.sh
@@ -9,4 +9,33 @@ else
     DEBUG="-v=4"
 fi
 
+function find_iptables_on_host() {
+    chroot /host test -x /usr/sbin/$1 && { echo "/usr/sbin"; return; }
+    chroot /host test -x /sbin/$1 && { echo "/sbin"; return; }
+    echo "unknown"
+}
+
+
+# if host is mounted on /host and host has it's own iptables version
+# use that one instead via the shipped iptables wrapper, we do this
+# to avoid configuring iptables and nftables on the host which
+# could lead to functional failures because some hosts use an iptables
+# and iptables-save which program nftables under the hood.
+
+for f in iptables-save iptables; do
+  iptablesLocation=$(find_iptables_on_host $f)
+  if [ $iptablesLocation == "/usr/sbin" ]; then
+    echo "$f is present on the host at /usr/sbin/$f"
+    cp /usr/sbin/${f}.wrapper /usr/sbin/$f
+  elif [ $iptablesLocation == "/sbin" ]; then
+    echo "$f is present on the host at /sbin/$f"
+    cp /usr/sbin/${f}.sbin.wrapper /usr/sbin/$f
+  else
+    echo "WARNING: not using iptables wrapper because iptables was not detected on the"
+    echo "host at the following paths [/usr/sbin, /sbin]."
+    echo "Either the host file system isn't mounted or the host does not have iptables"
+    echo "installed. The pod will use the image installed iptables version."
+  fi
+done
+
 exec submariner-globalnet ${DEBUG} -alsologtostderr
---
+++ pkg/globalnet/controllers/ipam/globalnet_ingress.go
@@ -0,0 +1,83 @@
+package ipam
+
+import (
+	"crypto/sha256"
+	"encoding/base32"
+	"fmt"
+	"strings"
+
+	"github.com/coreos/go-iptables/iptables"
+	"github.com/submariner-io/submariner/pkg/util"
+	k8sv1 "k8s.io/api/core/v1"
+	"k8s.io/klog"
+)
+
+func (i *Controller) initIPTableChains() error {
+	ipt, err := iptables.New()
+	if err != nil {
+		return fmt.Errorf("error initializing iptables: %v", err)
+	}
+
+	klog.V(4).Infof("Install/ensure %s chain exists", submarinerGlobalNet)
+	if err = util.CreateChainIfNotExists(ipt, "nat", submarinerGlobalNet); err != nil {
+		return fmt.Errorf("error creating iptables chain %s: %v", submarinerGlobalNet, err)
+	}
+
+	forwardToSubGlobalNetRuleSpec := []string{"-j", submarinerGlobalNet}
+	if err = util.PrependUnique(ipt, "nat", "PREROUTING", forwardToSubGlobalNetRuleSpec); err != nil {
+		klog.Errorf("error appending iptables rule \"%s\": %v\n", strings.Join(forwardToSubGlobalNetRuleSpec, " "), err)
+	}
+	return nil
+}
+
+func (i *Controller) updateIngressRulesForService(globalIP, chainName string, addOrDelete Operation) error {
+	ipt, err := iptables.New()
+	if err != nil {
+		return fmt.Errorf("error initializing iptables: %v", err)
+	}
+
+	ruleSpec := []string{"-d", globalIP, "-j", chainName}
+	if addOrDelete == AddRules {
+		klog.V(4).Infof("Installing iptables rule for Service %s", strings.Join(ruleSpec, " "))
+		if err = ipt.AppendUnique("nat", submarinerGlobalNet, ruleSpec...); err != nil {
+			return fmt.Errorf("error appending iptables rule \"%s\": %v\n", strings.Join(ruleSpec, " "), err)
+		}
+	} else if addOrDelete == DeleteRules {
+		klog.V(4).Infof("Deleting iptable ingress rule for Service: %s", strings.Join(ruleSpec, " "))
+		if err = ipt.Delete("nat", submarinerGlobalNet, ruleSpec...); err != nil {
+			return fmt.Errorf("error deleting iptables rule \"%s\": %v\n", strings.Join(ruleSpec, " "), err)
+		}
+	}
+	return nil
+}
+
+func (i *Controller) kubeProxyClusterIpServiceChainName(service *k8sv1.Service) string {
+	// CNIs that use kube-proxy with iptables for loadbalancing create an iptables chain for each service
+	// and incoming traffic to the clusterIP Service is directed into the respective chain.
+	// Reference: https://bit.ly/2OPhlwk
+	serviceName := service.GetNamespace() + "/" + service.GetName() + ":"
+	protocol := strings.ToLower(string(service.Spec.Ports[0].Protocol))
+	hash := sha256.Sum256([]byte(serviceName + protocol))
+	encoded := base32.StdEncoding.EncodeToString(hash[:])
+	return kubeProxyServiceChainPrefix + encoded[:16]
+}
+
+func (i *Controller) doesIPTablesChainExist(table, chain string) error {
+	ipt, err := iptables.New()
+	if err != nil {
+		return fmt.Errorf("error initializing iptables: %v", err)
+	}
+
+	existingChains, err := ipt.ListChains(table)
+	if err != nil {
+		return err
+	}
+
+	for _, val := range existingChains {
+		if val == chain {
+			klog.V(4).Infof("%s chain exists in %s table", chain, table)
+			return nil
+		}
+	}
+	return fmt.Errorf("chain %s does not exist", chain)
+}
