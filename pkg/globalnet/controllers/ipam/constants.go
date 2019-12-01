c0a2336b36bcae2472daca2b82a34b3aade157d9
Sridhar Gaddam <sgaddam@redhat.com>
2019-12-01 21:02:28 +0530

    Implement globalnet ingress and egress features
    This patch implements the following
    1. Spawns Globalnet Controller on the GatewayNode
    2. Initializes Globalnet Controller only when the Cluster uses kubeproxy
    3. Programs necessary egress rules for PODs
    4. Programs necessary ingress rules for ClusterIPServices
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pkg/globalnet/controllers/ipam/constants.go
+++ pkg/globalnet/controllers/ipam/constants.go
@@ -2,5 +2,25 @@ package ipam
 
 import "time"
 
-const handlerResync = time.Hour * 24
-const submarinerIpamGlobalIp = "submariner.io/globalIp"
+type Operation string
+
+const (
+	handlerResync          = time.Hour * 24
+	submarinerIpamGlobalIp = "submariner.io/globalIp"
+	submarinerGlobalNet    = "SUBMARINER-GLOBALNET"
+
+	// Currently Submariner Globalnet implementation (for services) works with kube-proxy
+	// and uses iptable chain-names programmed by kube-proxy. If the internal implementation
+	// of kube-proxy changes, globalnet needs to be modified accordingly.
+	// Reference: https://bit.ly/2OPhlwk
+	kubeProxyServiceChainPrefix = "KUBE-SVC-"
+	kubeProxyNameSpace          = "kube-system"
+	kubeProxyLabelSelector      = "k8s-app=kube-proxy"
+
+	AddRules    = true
+	DeleteRules = false
+
+	Process = "Process"
+	Ignore  = "Ignore"
+	Requeue = "Requeue"
+)
