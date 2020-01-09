0ee391bcb1e6c566a128b305b6604b6c77f988b4
Sridhar Gaddam <sgaddam@redhat.com>
2020-01-09 21:12:03 +0530

    Fix build issues on the overlapping-cidrs branch (#278)
    This PR fixes two issues.
    
    1. Patch [https://github.com/submariner-io/submariner/pull/251] removed an
       API from ippool, but the associated unit tests were not updated. This
       patch fixes those issues.
    2. PR [https://github.com/submariner-io/submariner/pull/232] modified the
       iptable wrappers. Similar changes are required in the globalnet controller.
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- package/submariner-globalnet.sh
+++ package/submariner-globalnet.sh
@@ -16,20 +16,21 @@ function find_iptables_on_host() {
 }
 
 
-# if host is mounted on /host and host has it's own iptables version
+# If host is mounted on /host and host has its own iptables version
 # use that one instead via the shipped iptables wrapper, we do this
 # to avoid configuring iptables and nftables on the host which
 # could lead to functional failures because some hosts use an iptables
 # and iptables-save which program nftables under the hood.
+# Since we're using UBI8, we only have nftables in the container so we
+# can't use the Kubernetes approach (see
+# https://github.com/kubernetes/kubernetes/pull/82966 and
+# https://github.com/kubernetes/website/pull/16271 for details).
 
 for f in iptables-save iptables; do
-  iptablesLocation=$(find_iptables_on_host $f)
-  if [ $iptablesLocation == "/usr/sbin" ]; then
-    echo "$f is present on the host at /usr/sbin/$f"
-    cp /usr/sbin/${f}.wrapper /usr/sbin/$f
-  elif [ $iptablesLocation == "/sbin" ]; then
-    echo "$f is present on the host at /sbin/$f"
-    cp /usr/sbin/${f}.sbin.wrapper /usr/sbin/$f
+  location=$(find_iptables_on_host $f)
+  if [ "${location}" != "unknown" ]; then
+    echo "$f is present on the host at ${location}/$f"
+	sed "s!@@PATH@@!${location}!" /usr/sbin/iptables-wrapper.in > /usr/sbin/$f
   else
     echo "WARNING: not using iptables wrapper because iptables was not detected on the"
     echo "host at the following paths [/usr/sbin, /sbin]."
