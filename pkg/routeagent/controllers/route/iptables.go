14d3d7dc7c01c5d2811e1c86a8a4b85a2b92e019
Sridhar Gaddam <sgaddam@redhat.com>
2020-01-29 17:42:38 +0530

    Support Connectivity from HostNetwork to RemoteCluster
    Currently, Submariner configures necessary tunnels and routing rules to
    support connectivity between the PODs in different Clusters. However, if
    we try to connect from the underlying host to a remotePOD/Service it does
    not work. While working on some of the Storage use-cases in K8s, we realized
    that connectivity from underlying Host to remoteCluster is required. This
    patch addresses this use-case.
    
    Fixes Issue# https://github.com/submariner-io/submariner/issues/298
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pkg/routeagent/controllers/route/iptables.go
+++ pkg/routeagent/controllers/route/iptables.go
@@ -18,35 +18,45 @@ func (r *Controller) createIPTableChains() error {
 
 	klog.V(4).Infof("Install/ensure %s chain exists", SmPostRoutingChain)
 	if err = util.CreateChainIfNotExists(ipt, "nat", SmPostRoutingChain); err != nil {
-		return fmt.Errorf("Unable to create %s chain in iptables: %v", SmPostRoutingChain, err)
+		return fmt.Errorf("unable to create %s chain in iptables: %v", SmPostRoutingChain, err)
 	}
 
 	klog.V(4).Infof("Insert %s rule that has rules for inter-cluster traffic", SmPostRoutingChain)
 	forwardToSubPostroutingRuleSpec := []string{"-j", SmPostRoutingChain}
 	if err = util.PrependUnique(ipt, "nat", "POSTROUTING", forwardToSubPostroutingRuleSpec); err != nil {
-		klog.Errorf("Unable to insert iptable rule in NAT table, POSTROUTING chain: %v", err)
+		return fmt.Errorf("unable to insert iptable rule in NAT table, POSTROUTING chain: %v", err)
 	}
 
 	klog.V(4).Infof("Install/ensure SUBMARINER-INPUT chain exists")
 	if err = util.CreateChainIfNotExists(ipt, "filter", "SUBMARINER-INPUT"); err != nil {
-		return fmt.Errorf("Unable to create SUBMARINER-INPUT chain in iptables: %v", err)
+		return fmt.Errorf("unable to create SUBMARINER-INPUT chain in iptables: %v", err)
 	}
 
 	forwardToSubInputRuleSpec := []string{"-p", "udp", "-m", "udp", "-j", "SUBMARINER-INPUT"}
 	if err = ipt.AppendUnique("filter", "INPUT", forwardToSubInputRuleSpec...); err != nil {
-		klog.Errorf("Unable to append iptables rule \"%s\": %v\n", strings.Join(forwardToSubInputRuleSpec, " "), err)
+		return fmt.Errorf("unable to append iptables rule %q: %v\n", strings.Join(forwardToSubInputRuleSpec, " "), err)
 	}
 
 	klog.V(4).Infof("Allow VxLAN incoming traffic in SUBMARINER-INPUT Chain")
 	ruleSpec := []string{"-p", "udp", "-m", "udp", "--dport", strconv.Itoa(VxLANPort), "-j", "ACCEPT"}
 	if err = ipt.AppendUnique("filter", "SUBMARINER-INPUT", ruleSpec...); err != nil {
-		klog.Errorf("Unable to append iptables rule \"%s\": %v\n", strings.Join(ruleSpec, " "), err)
+		return fmt.Errorf("unable to append iptables rule %q: %v\n", strings.Join(ruleSpec, " "), err)
 	}
 
 	klog.V(4).Infof("Insert rule to allow traffic over %s interface in FORWARDing Chain", VxLANIface)
 	ruleSpec = []string{"-o", VxLANIface, "-j", "ACCEPT"}
 	if err = util.PrependUnique(ipt, "filter", "FORWARD", ruleSpec); err != nil {
-		klog.Errorf("Unable to insert iptable rule in filter table to allow vxlan traffic: %v", err)
+		return fmt.Errorf("unable to insert iptable rule in filter table to allow vxlan traffic: %v", err)
+	}
+
+	if r.cniIface != nil {
+		// Program rules to support communication from HostNetwork to remoteCluster
+		sourceAddress := strconv.Itoa(VxLANVTepNetworkPrefix) + ".0.0.0/8"
+		ruleSpec = []string{"-s", sourceAddress, "-o", VxLANIface, "-j", "SNAT", "--to", r.cniIface.ipAddress}
+		klog.V(4).Infof("Installing rule for host network to remote cluster communication: %s", strings.Join(ruleSpec, " "))
+		if err = ipt.AppendUnique("nat", SmPostRoutingChain, ruleSpec...); err != nil {
+			return fmt.Errorf("error appending iptables rule %q: %v\n", strings.Join(ruleSpec, " "), err)
+		}
 	}
 
 	return nil
