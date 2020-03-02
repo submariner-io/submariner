818ac1fd899db642082c3977e52a62cdb9905c6d
Tom Pantelis <tompantelis@gmail.com>
2020-03-02 15:40:56 -0500

    Remove unused functions in Datastore interface
    Simplifies implementations and unit tests. We can add functions
    back if ever needed.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/datastore/datastore.go
+++ pkg/datastore/datastore.go
@@ -15,17 +15,8 @@ type OnClusterChange func(cluster *types.SubmarinerCluster, deleted bool) error
 type OnEndpointChange func(endpoint *types.SubmarinerEndpoint, deleted bool) error
 
 type Datastore interface {
-	// This gets all clusters that match the color code
-	GetClusters(colorCodes []string) ([]types.SubmarinerCluster, error)
-
-	// This gets a single cluster based on cluster ID and returns a v1.Cluster or error based on what it retrieves
-	GetCluster(clusterID string) (*types.SubmarinerCluster, error)
-
 	// This gets all endpoints for the given cluster ID
 	GetEndpoints(clusterID string) ([]types.SubmarinerEndpoint, error)
-
-	// This gets a single endpoint based on the cluster ID and cableName passed in
-	GetEndpoint(clusterID string, cableName string) (*types.SubmarinerEndpoint, error)
 
 	// Watches all clusters and calls the passed in function on cluster change
 	WatchClusters(ctx context.Context, selfClusterID string, colorCodes []string, onClusterChange OnClusterChange) error
@@ -41,7 +32,4 @@ type Datastore interface {
 
 	// This should be called to remove an endpoint from use
 	RemoveEndpoint(clusterID, cableName string) error
-
-	// This should be called to remove a cluster from use
-	RemoveCluster(clusterID string) error
 }
