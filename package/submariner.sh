aba7f798f1433581c7f5fa4883e4bf694549e33d
Sridhar Gaddam <sgaddam@redhat.com>
2020-04-17 18:22:12 +0530

    Fix log levels in Submariner Gateway and Globalnet
    Recently log levels were updated in Submariner code to use one
    of INFO/DEBUG/TRACE. This PR modifies the defaults to use DEBUG
    which otherwise were logging at TRACE.
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- package/submariner.sh
+++ package/submariner.sh
@@ -6,10 +6,12 @@ trap "exit 1" SIGTERM SIGINT
 export CHARON_PID_FILE=/var/run/charon.pid
 rm -f ${CHARON_PID_FILE}
 
+SUBMARINER_VERBOSITY=${SUBMARINER_VERBOSITY:-2}
+
 if [ "${SUBMARINER_DEBUG}" == "true" ]; then
-    DEBUG="-v=9"
+    DEBUG="-v=3"
 else
-    DEBUG="-v=4"
+    DEBUG="-v=${SUBMARINER_VERBOSITY}"
 fi
 
 mkdir -p /etc/ipsec
