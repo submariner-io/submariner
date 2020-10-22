da6a618bfa67cce22ae439351f16b18db82576d4
Miguel Angel Ajo <majopela@redhat.com>
2020-10-22 14:04:37 +0200

    Add submariner-networkplugin-syncer stub
    This adds a simple image and binary for the submariner-networkplugin-syncer
    which will be used to test the eventhandler controller which will be built
    on next commits.
    
    Co-Authored-by: Sridhar Gaddam <sgaddam@redhat.com>
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

---
+++ package/submariner-networkplugin-syncer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e -x
+
+trap "exit 1" SIGTERM SIGINT
+
+SUBMARINER_VERBOSITY=${SUBMARINER_VERBOSITY:-2}
+
+if [ "${SUBMARINER_DEBUG}" == "true" ]; then
+    DEBUG="-v=3"
+else
+    DEBUG="-v=${SUBMARINER_VERBOSITY}"
+fi
+
+exec submariner-networkplugin-sync ${DEBUG} -alsologtostderr
