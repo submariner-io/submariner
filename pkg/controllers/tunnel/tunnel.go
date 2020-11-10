7c6a09f75d4f7349d523fae59c507d21e93bca4e
Tom Pantelis <tompantelis@gmail.com>
2020-11-09 22:29:37 -0500

    Convert tunnel controller to use resource watcher
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/controllers/tunnel/tunnel.go
+++ pkg/controllers/tunnel/tunnel.go
@@ -1,15 +1,12 @@
 package tunnel
 
 import (
-	"github.com/submariner-io/admiral/pkg/federate"
 	"github.com/submariner-io/admiral/pkg/log"
-	"github.com/submariner-io/admiral/pkg/syncer"
+	"github.com/submariner-io/admiral/pkg/watcher"
 	v1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
 	"github.com/submariner-io/submariner/pkg/cableengine"
 	"github.com/submariner-io/submariner/pkg/types"
-	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/dynamic"
 	"k8s.io/klog"
 )
 
@@ -17,28 +14,30 @@ type controller struct {
 	engine cableengine.Engine
 }
 
-func StartController(engine cableengine.Engine, namespace string, client dynamic.Interface, restMapper meta.RESTMapper,
-	scheme *runtime.Scheme, stopCh <-chan struct{}) error {
+func StartController(engine cableengine.Engine, namespace string, config *watcher.Config, stopCh <-chan struct{}) error {
 	klog.Info("Starting the tunnel controller")
 
 	c := &controller{engine: engine}
 
-	endpointSyncer, err := syncer.NewResourceSyncer(&syncer.ResourceSyncerConfig{
-		Name:            "Tunnel controller",
-		SourceClient:    client,
-		SourceNamespace: namespace,
-		Direction:       syncer.RemoteToLocal,
-		RestMapper:      restMapper,
-		Federator:       federate.NewNoopFederator(),
-		ResourceType:    &v1.Endpoint{},
-		Transform:       c.processEndpoint,
-		Scheme:          scheme,
-	})
+	config.ResourceConfigs = []watcher.ResourceConfig{
+		{
+			Name:         "Tunnel Controller",
+			ResourceType: &v1.Endpoint{},
+			Handler: watcher.EventHandlerFuncs{
+				OnCreateFunc: c.handleCreatedOrUpdatedEndpoint,
+				OnUpdateFunc: c.handleCreatedOrUpdatedEndpoint,
+				OnDeleteFunc: c.handleRemovedEndpoint,
+			},
+			SourceNamespace: namespace,
+		},
+	}
+
+	endpointWatcher, err := watcher.New(config)
 	if err != nil {
 		return err
 	}
 
-	err = endpointSyncer.Start(stopCh)
+	err = endpointWatcher.Start(stopCh)
 	if err != nil {
 		return err
 	}
@@ -46,12 +45,8 @@ func StartController(engine cableengine.Engine, namespace string, client dynamic
 	return nil
 }
 
-func (c *controller) processEndpoint(obj runtime.Object, op syncer.Operation) (runtime.Object, bool) {
+func (c *controller) handleCreatedOrUpdatedEndpoint(obj runtime.Object) bool {
 	endpoint := obj.(*v1.Endpoint)
-
-	if op == syncer.Delete {
-		return nil, c.handleRemovedEndpoint(endpoint)
-	}
 
 	klog.V(log.DEBUG).Infof("Tunnel controller processing added or updated submariner Endpoint object: %#v", endpoint)
 
@@ -62,15 +57,17 @@ func (c *controller) processEndpoint(obj runtime.Object, op syncer.Operation) (r
 	err := c.engine.InstallCable(myEndpoint)
 	if err != nil {
 		klog.Errorf("error installing cable for Endpoint %#v, %v", myEndpoint, err)
-		return nil, true
+		return true
 	}
 
 	klog.V(log.DEBUG).Infof("Tunnel controller successfully installed Endpoint cable %s in the engine", endpoint.Spec.CableName)
 
-	return nil, false
+	return false
 }
 
-func (c *controller) handleRemovedEndpoint(endpoint *v1.Endpoint) bool {
+func (c *controller) handleRemovedEndpoint(obj runtime.Object) bool {
+	endpoint := obj.(*v1.Endpoint)
+
 	klog.V(log.DEBUG).Infof("Tunnel controller processing removed submariner Endpoint object: %#v", endpoint)
 
 	myEndpoint := types.SubmarinerEndpoint{
