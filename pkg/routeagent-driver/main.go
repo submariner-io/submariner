9a384f7f80fed26ab855260b710acf72e3ba87b6
Sridhar Gaddam <sgaddam@redhat.com>
2020-11-17 19:53:16 +0530

    Annotate the node with cniInterfaceIP
    This PR reads the NODE_NAME from the pod environment variables
    using K8s downward API and annotates the node with the respective
    cniIfaceIP used on the node.
    
    Signed-Off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pkg/routeagent-driver/main.go
+++ pkg/routeagent-driver/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/kelseyhightower/envconfig"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 
 	"github.com/submariner-io/submariner/pkg/event"
 	"github.com/submariner-io/submariner/pkg/event/controller"
 	"github.com/submariner-io/submariner/pkg/event/logger"
+	"github.com/submariner-io/submariner/pkg/routeagent-driver/cni_interface"
 	"github.com/submariner-io/submariner/pkg/routeagent-driver/constants"
 	kp_iptables "github.com/submariner-io/submariner/pkg/routeagent-driver/handlers/kubeproxy-iptables"
 )
@@ -32,6 +36,10 @@ func main() {
 	err := envconfig.Process("submariner", &env)
 	if err != nil {
 		klog.Fatalf("Error reading the environment variables: %s", err.Error())
+	}
+
+	if err = annotateNode(env.ClusterCidr); err != nil {
+		klog.Errorf("Error while annotating the node: %s", err.Error())
 	}
 
 	eventHandlers := event.NewRegistry("routeagent-driver", os.Getenv("NETWORK_PLUGIN"))
@@ -63,4 +71,28 @@ func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&masterURL, "master", "",
 		"The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
+}
+
+func annotateNode(clusterCidr []string) error {
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	clientSet, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building clientset: %s", err.Error())
+	}
+
+	nodeName, ok := os.LookupEnv("NODE_NAME")
+	if !ok {
+		return fmt.Errorf("Error reading the NODE_NAME from the environment")
+	}
+
+	err = cni_interface.AnnotateNodeWithCNIInterfaceIP(nodeName, constants.CniInterfaceIp, clientSet, clusterCidr)
+	if err != nil {
+		return fmt.Errorf("AnnotateNodeWithCNIInterfaceIP returned error %v", err)
+	}
+
+	return nil
 }
