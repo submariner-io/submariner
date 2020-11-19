1accf0045481d94c1ce3019fe82b46e6517d72b4
Sridhar Gaddam <sgaddam@redhat.com>
2020-11-19 23:52:35 +0530

    Handle overlapping CIDRs from remote clusters
    If subnets of remote clusters overlap with local cluster, skip the
    endpoint and avoid making any changes on the cluster nodes as it will
    disrupt the local cluster.
    
    Signed-Off-by: Sridhar Gaddam <sgaddam@redhat.com>

---
+++ pkg/routeagent-driver/constants/constants.go
@@ -0,0 +1,32 @@
+package constants
+
+const (
+	SmGlobalnetIngressChain = "SUBMARINER-GN-INGRESS"
+	SmGlobalnetEgressChain  = "SUBMARINER-GN-EGRESS"
+	SmGlobalnetMarkChain    = "SUBMARINER-GN-MARK"
+
+	// IPTable chains used by RouteAgent
+	SmPostRoutingChain = "SUBMARINER-POSTROUTING"
+
+	// In order to support connectivity from HostNetwork to remoteCluster, route-agent tries
+	// to discover the CNIInterface[#] on the respective node and does SNAT of outgoing
+	// traffic from that node to the corresponding CNIInterfaceIP. It is to be noted that
+	// only traffic destined to the remoteClusters connected via Submariner is SNAT'ed and not
+	// any other traffic.
+	// At the same time, when Globalnet controller is deployed (i.e., clusters with overlapping
+	// Service/Cluster CIDRs) it needs this information so that it can map the CNIInterfaceIP
+	// with the corresponding globalIP assigned to the node. Since globalnet controller does
+	// not run on all the worker-nodes and there is no well defined mechanism to get the
+	// CNIInterfaceIP for each of the nodes, we annotate the node with CNIInterfaceIPInfo as
+	// part of route-agent and this will subsequently be used in globalnet controller for
+	// supporting connectivity from HostNetwork to remoteClusters.
+	// [#] interface on the node that has an IPAddress from the clusterCIDR
+	CniInterfaceIp = "submariner.io/cniIfaceIp"
+)
+
+type Specification struct {
+	ClusterID   string
+	Namespace   string
+	ClusterCidr []string
+	ServiceCidr []string
+}
--- pkg/routeagent-driver/handlers/kubeproxy-iptables/endpoint_handler.go
+++ pkg/routeagent-driver/handlers/kubeproxy-iptables/endpoint_handler.go
@@ -1,11 +1,14 @@
 package kp_iptables
 
 import (
+	"fmt"
+
 	"k8s.io/klog"
 
 	"github.com/submariner-io/admiral/pkg/log"
 
 	submV1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
+	"github.com/submariner-io/submariner/pkg/util"
 )
 
 func (kp *SyncHandler) LocalEndpointCreated(endpoint *submV1.Endpoint) error {
@@ -28,17 +31,62 @@ func (kp *SyncHandler) LocalEndpointRemoved(endpoint *submV1.Endpoint) error {
 func (kp *SyncHandler) RemoteEndpointCreated(endpoint *submV1.Endpoint) error {
 	klog.V(log.DEBUG).Infof("A new Endpoint for remote cluster %q has been created: %#v",
 		endpoint.Spec.ClusterID, endpoint)
+	if err := kp.overlappingSubnets(endpoint.Spec.Subnets); err != nil {
+		// Skip processing the endpoint when CIDRs overlap
+		return err
+	}
+
 	return nil
 }
 
 func (kp *SyncHandler) RemoteEndpointUpdated(endpoint *submV1.Endpoint) error {
 	klog.V(log.DEBUG).Infof("A new Endpoint for remote cluster %q has been updated: %#v",
 		endpoint.Spec.ClusterID, endpoint)
+	if err := kp.overlappingSubnets(endpoint.Spec.Subnets); err != nil {
+		// Skip processing the endpoint when CIDRs overlap
+		return err
+	}
+
 	return nil
 }
 
 func (kp *SyncHandler) RemoteEndpointRemoved(endpoint *submV1.Endpoint) error {
 	klog.V(log.DEBUG).Infof("A new Endpoint for remote cluster %q has been removed: %#v",
 		endpoint.Spec.ClusterID, endpoint)
+	return nil
+}
+
+func (kp *SyncHandler) overlappingSubnets(remoteSubnets []string) error {
+	// If the remoteSubnets [*] overlap with local cluster Pod/Service CIDRs we
+	// should not update the IPTable rules on the host, as it will disrupt the
+	// functionality of the local cluster. So, lets validate that subnets do not
+	// overlap before we program any IPTable rules on the host for inter-cluster
+	// traffic.
+	// [*] Note: In a non-GlobalNet deployment, remoteSubnets will be a list of
+	// Pod/Service CIDRs, whereas in a GlobalNet deployment, it will be a list of
+	// globalCIDRs allocated to the clusters.
+	for _, serviceCidr := range kp.localServiceCidr {
+		overlap, err := util.IsOverlappingCIDR(remoteSubnets, serviceCidr)
+		if err != nil {
+			// Ideally this case will never hit, as the subnets are valid CIDRs
+			klog.Warningf("unable to validate overlapping Service CIDR: %s", err)
+		}
+
+		if overlap {
+			return fmt.Errorf("Local Service CIDR %q, overlaps with remote cluster %s", serviceCidr, err)
+		}
+	}
+
+	for _, podCidr := range kp.localClusterCidr {
+		overlap, err := util.IsOverlappingCIDR(remoteSubnets, podCidr)
+		if err != nil {
+			klog.Warningf("unable to validate overlapping Pod CIDR: %s", err)
+		}
+
+		if overlap {
+			return fmt.Errorf("Local Pod CIDR %q, overlaps with remote cluster %s", podCidr, err)
+		}
+	}
+
 	return nil
 }
