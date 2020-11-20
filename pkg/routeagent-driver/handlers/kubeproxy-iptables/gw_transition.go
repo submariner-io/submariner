5025f0ad27acd046f220b69af3e49442a0f90430
Sridhar Gaddam <sgaddam@redhat.com>
2020-11-20 20:56:21 +0530

    Implement vxLAN overlay tunnels to preserve the sourceIP
    This PR implements the following features
    1. Creates vxLan interfaces between the worker nodes and the
       local active gateway node.
    2. Handles gateway failover scenarios
    3. Handles programming of necessary routes and iptable rules
       when a new endpoint is added
    4. Programs the FDB entries on the GatewayNode when a new node
       is added to the cluster.
    
    Fixes issue: https://github.com/submariner-io/submariner/issues/858
    Signed-Off-by: Sridhar Gaddam <sgaddam@redhat.com>

--- pkg/routeagent-driver/handlers/kubeproxy-iptables/gw_transition.go
+++ pkg/routeagent-driver/handlers/kubeproxy-iptables/gw_transition.go
@@ -11,24 +11,42 @@ func (kp *SyncHandler) TransitionToNonGateway() error {
 	kp.syncHandlerMutex.Lock()
 	defer kp.syncHandlerMutex.Unlock()
 	kp.isGatewayNode = false
-	kp.updateRoutingRulesForHostNetworkSupport(nil, FlushRouteTable)
+
+	kp.cleanVxSubmarinerRoutes()
+	// If the active Gateway transitions to a new node, we flush the HostNetwork routing table.
+	kp.updateRoutingRulesForHostNetworkSupport(nil, Flush)
+	err := kp.configureIPRule(Delete)
+	if err != nil {
+		klog.Errorf("Unable to delete ip rule to table %d on non-Gateway node %s: %v",
+			RouteAgentHostNetworkTableID, kp.hostname, err)
+	}
 
 	return nil
 }
 
 func (kp *SyncHandler) TransitionToGateway() error {
 	klog.V(log.DEBUG).Info("The current node has become a Gateway")
+	kp.cleanVxSubmarinerRoutes()
+
 	kp.syncHandlerMutex.Lock()
 	defer kp.syncHandlerMutex.Unlock()
 	kp.isGatewayNode = true
+
+	klog.Infof("Creating the vxlan interface: %s on the gateway node", VxLANIface)
+
+	err := kp.createVxLANInterface(VxInterfaceGateway, nil)
+	if err != nil {
+		klog.Fatalf("Unable to create VxLAN interface on gateway node (%s): %v", kp.hostname, err)
+	}
+
+	err = kp.configureIPRule(Add)
+	if err != nil {
+		klog.Errorf("Unable to add ip rule to table %d on Gateway node %s: %v",
+			RouteAgentHostNetworkTableID, kp.hostname, err)
+	}
+
 	// Add routes to the new endpoint on the GatewayNode.
-	kp.updateRoutingRulesForHostNetworkSupport(kp.remoteSubnets.Elements(), AddRoute)
+	kp.updateRoutingRulesForHostNetworkSupport(kp.remoteSubnets.Elements(), Add)
 
 	return nil
-}
-
-func (kp *SyncHandler) populateRemoteVtepIps(vtepIP string) {
-	if !kp.remoteVTEPs.Contains(vtepIP) {
-		kp.remoteVTEPs.Add(vtepIP)
-	}
 }
