84c811645befe5e6c57f6f69a580c65573208402
Miguel Angel Ajo <majopela@redhat.com>
2020-11-20 17:35:10 +0100

    Address review comments for cluster_files
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- pkg/util/cluster_files/cluster_files.go
+++ pkg/util/cluster_files/cluster_files.go
@@ -14,12 +14,6 @@ import (
 	"k8s.io/klog"
 )
 
-var (
-	UnsupportedSchema   error = errors.New("The schema is not supported by cluster_files module")
-	MalformedURL        error = errors.New("The url is not well formed for cluster_files module")
-	FileContentNotFound error = errors.New("File data not found in object")
-)
-
 // Get retrieves a config from a secret, configmap or file within the k8s cluster
 // using an url schema that supports configmap://<namespace>/<configmap-name>/<data-file>
 // secret://<namespace>/<secret-name>/<data-file> and file:///<path> returning
@@ -28,8 +22,7 @@ func Get(k8sClient kubernetes.Interface, urlAddress string) (pathStr string, err
 	klog.V(log.DEBUG).Infof("reading cluster_file: %s", urlAddress)
 	parsedUrl, err := url.Parse(urlAddress)
 	if err != nil {
-		klog.Error(err)
-		return "", errors.Wrapf(err, "Trying to read cluster file %q", urlAddress)
+		return "", errors.Wrapf(err, "error parsing cluster file URL %q", urlAddress)
 	}
 
 	namespace := parsedUrl.Host
@@ -37,7 +30,7 @@ func Get(k8sClient kubernetes.Interface, urlAddress string) (pathStr string, err
 	pathContainerObject = strings.Trim(pathContainerObject, "/")
 
 	if pathContainerObject == "" || pathFile == "" {
-		return "", MalformedURL
+		return "", errors.Errorf("cluster file URL %q is not well formed", urlAddress)
 	}
 
 	var data []byte
@@ -49,32 +42,32 @@ func Get(k8sClient kubernetes.Interface, urlAddress string) (pathStr string, err
 	case "secret":
 		secret, err := k8sClient.CoreV1().Secrets(namespace).Get(pathContainerObject, metav1.GetOptions{})
 		if err != nil {
-			return "", errors.Wrapf(err, "Error reading secret %q from namespace %q", pathContainerObject, namespace)
+			return "", errors.Wrapf(err, "error reading secret %q from namespace %q", pathContainerObject, namespace)
 		}
 		var ok bool
 		data, ok = secret.Data[pathFile]
 		if !ok {
-			return "", FileContentNotFound
+			return "", errors.Errorf("cluster file data %q not found in secret %s", pathFile, secret.Name)
 		}
 
 	case "configmap":
 		configMap, err := k8sClient.CoreV1().ConfigMaps(namespace).Get(pathContainerObject, metav1.GetOptions{})
 		if err != nil {
-			return "", errors.Wrapf(err, "Error reading configmap %q from namespace %q", pathContainerObject, namespace)
+			return "", errors.Wrapf(err, "error reading configmap %q from namespace %q", pathContainerObject, namespace)
 		}
 		var ok bool
 		data, ok = configMap.BinaryData[pathFile]
 		if !ok {
 			dataStr, ok := configMap.Data[pathFile]
 			if !ok {
-				return "", FileContentNotFound
+				return "", errors.Errorf("cluster file data %q not found in %#v", pathFile, configMap)
 			}
 
 			data = []byte(dataStr)
 		}
 
 	default:
-		return "", UnsupportedSchema
+		return "", errors.Errorf("the scheme %q in cluster file URL %q is not supported ", parsedUrl.Scheme, urlAddress)
 	}
 
 	return storeToDisk(pathContainerObject, parsedUrl, data)
@@ -83,17 +76,20 @@ func Get(k8sClient kubernetes.Interface, urlAddress string) (pathStr string, err
 func storeToDisk(pathContainerObject string, parsedUrl *url.URL, data []byte) (string, error) {
 	storageDirectory, err := ioutil.TempDir("", "cluster_files")
 	if err != nil {
-		klog.Error(err)
-		return "", errors.Wrap(err, "Error creating cluster_files directory")
+		return "", errors.Wrap(err, "error creating cluster_files directory")
 	}
 
-	dir := path.Join(storageDirectory, pathContainerObject)
-	_ = os.MkdirAll(dir, 0700)
 	diskFilePath := path.Join(storageDirectory, parsedUrl.Path)
+	dir := path.Join(storageDirectory, pathContainerObject)
+	err = os.MkdirAll(dir, 0700)
+	if err != nil {
+		return "", errors.Wrapf(err, "error creating %s directory to store %s", dir, diskFilePath)
+	}
+
 	err = ioutil.WriteFile(diskFilePath, data, 0400)
 	if err != nil {
 		klog.Error(err)
-		return "", errors.Wrapf(err, "Error writing file to storage: %q", err)
+		return "", errors.Wrapf(err, "error writing cluster file to  %q", diskFilePath)
 	}
 
 	return diskFilePath, nil
