9e17e95ece0ef04550a9eee969a7de02a8127dd4
Maayan Friedman <maafried@redhat.com>
2020-11-30 15:36:22 +0200

    Record connection latency metric
    Related issue #678
    
    Signed-off-by: Maayan Friedman <maafried@redhat.com>

--- pkg/cableengine/syncer/syncer.go
+++ pkg/cableengine/syncer/syncer.go
@@ -10,6 +10,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/submariner-io/admiral/pkg/log"
 	v1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
+	"github.com/submariner-io/submariner/pkg/cable"
 	"github.com/submariner-io/submariner/pkg/cableengine"
 	"github.com/submariner-io/submariner/pkg/cableengine/healthchecker"
 	v1typed "github.com/submariner-io/submariner/pkg/client/clientset/versioned/typed/submariner.io/v1"
@@ -209,6 +210,9 @@ func (i *GatewaySyncer) generateGatewayObject() *v1.Gateway {
 				latencyInfo := i.healthCheck.GetLatencyInfo(&connection.Endpoint)
 				if latencyInfo != nil {
 					connection.LatencyRTT = latencyInfo.Spec
+					lastRTT, _ := time.ParseDuration(latencyInfo.Spec.Last)
+					cable.RecordConnectionLatency(localEndpoint.Spec.CableName, &localEndpoint.Spec, &connection.Endpoint, lastRTT.Seconds())
+
 					if connection.Status == v1.Connected && latencyInfo.ConnectionError != "" {
 						connection.Status = v1.ConnectionError
 						connection.StatusMessage = latencyInfo.ConnectionError
