906d9ac861d886244c9664ec67099a03a6688b82
Aswin Surayanarayanan <asuryana@redhat.com>
2020-11-30 18:08:07 +0530

    Apply review comments
    Signed-off-by: Aswin Surayanarayanan <asuryana@redhat.com>

--- pkg/cableengine/healthchecker/pinger.go
+++ pkg/cableengine/healthchecker/pinger.go
@@ -11,7 +11,6 @@ import (
 var waitTime = 15 * time.Second
 
 const threshold = 5
-const allpackets = 100
 
 // The RTT will be stored and will be used to calculate the statistics until
 // the size is reached. Once the size is reached the array will be reset and
@@ -65,12 +64,11 @@ func (p *pingerInfo) sendPing() {
 	pinger.RecordRtts = false
 
 	pinger.OnSend = func(packet *ping.Packet) {
-		// Pinger will be stopped once the packet loss reaches threshold and
-		// if packet loss is not 100% recreates the Pinger without marking an error
-		if pinger.Statistics().PacketsSent-pinger.Statistics().PacketsRecv > threshold {
-			klog.Infof("Packet loss reached a threshold of %d and hence stopping the pinger for HealthCheckIP: %q",
-				threshold, p.healthCheckIP)
-			pinger.Stop()
+		// Pinger will mark a connection as an error if the packet loss reaches the threshold
+		if pinger.PacketsSent-pinger.PacketsRecv > threshold {
+			p.failureMsg = fmt.Sprintf("Failed to successfully ping the remote endpoint IP %q", p.healthCheckIP)
+			pinger.PacketsSent = 0
+			pinger.PacketsRecv = 0
 		}
 	}
 
@@ -79,13 +77,6 @@ func (p *pingerInfo) sendPing() {
 		p.statistics.update(uint64(packet.Rtt.Nanoseconds()))
 	}
 
-	pinger.OnFinish = func(stats *ping.Statistics) {
-		// Since we are setting a timeout and not a count, it will be an endless ping.
-		// If the timeout is reached with no successful packets, onFinish will be called and it is a failed ping.
-		if stats.PacketLoss == allpackets {
-			p.failureMsg = fmt.Sprintf("Failed to successfully ping the remote endpoint IP %q", p.healthCheckIP)
-		}
-	}
 	err = pinger.Run()
 	if err != nil {
 		klog.Errorf("Error running ping for the remote endpoint IP %q: %v", p.healthCheckIP, err)
