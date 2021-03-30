194a51d47dd2fbfc57fb18181ddb81eb26660a09
Miguel Angel Ajo <majopela@redhat.com>
2021-03-30 14:57:23 +0200

    Implement multiple connection modes in libreswan
    1) Bi-directional mode, if the other cluster is running 0.8 without this
       mode, or if none of them have a preference to be server we are
       conservative for now and keep the old behavior
    2) Server mode, if the side is preferred to be server, or if none
       is preferred but the break-tie algorithm wins server
    3) Client mode, if the side is not preferred to be server, or if none
       is preferred ant the break-tie algorithm wins client
    
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>
    Co-authored-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ pkg/cable/libreswan/preferred_server.go
@@ -0,0 +1,76 @@
+/*
+© 2021 Red Hat, Inc. and others
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package libreswan
+
+import (
+	v1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
+	"github.com/submariner-io/submariner/pkg/types"
+
+	"k8s.io/klog"
+)
+
+type operationMode int
+
+const (
+	operationModeBidirectional operationMode = iota
+	operationModeServer
+	operationModeClient
+)
+
+func (i *libreswan) calculateOperationMode(remoteEndpoint *types.SubmarinerEndpoint) operationMode {
+	defaultValue := false
+	leftPreferred, err := i.localEndpoint.Spec.GetBackendBool(v1.PreferredServerConfig, &defaultValue)
+	if err != nil {
+		klog.Errorf("Error parsing local endpoint config: %s", err)
+	}
+
+	rightPreferred, err := remoteEndpoint.Spec.GetBackendBool(v1.PreferredServerConfig, nil)
+	if err != nil {
+		klog.Errorf("Error parsing remote endpoint config %q: %s", remoteEndpoint.Spec.CableName, err)
+	}
+
+	if rightPreferred == nil || !*leftPreferred && !*rightPreferred {
+		return operationModeBidirectional
+	}
+
+	if *leftPreferred && !*rightPreferred {
+		return operationModeServer
+	}
+
+	if *rightPreferred && !*leftPreferred {
+		return operationModeClient
+	}
+
+	// At this point both would like to be server, so we decide based on the cable name
+	if i.localEndpoint.Spec.CableName > remoteEndpoint.Spec.CableName {
+		return operationModeServer
+	}
+
+	return operationModeClient
+}
+
+func (m operationMode) String() string {
+	switch m {
+	case operationModeBidirectional:
+		return "bi-directional"
+	case operationModeServer:
+		return "server"
+	case operationModeClient:
+		return "client"
+	default:
+		return "unknown"
+	}
+}
