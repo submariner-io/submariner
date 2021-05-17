2e9639693a2f6f4ba5949d375f049081a18f055c
Stephen Kitt <skitt@redhat.com>
2021-05-17 09:48:34 +0200

    Update the copyright statements to match CNCF recs
    This updates the copyright statements to match the CNCF
    recommendation, as per
    https://github.com/cncf/foundation/blob/master/copyright-notices.md
    
    We also add SPDX headers, and add enforcement (ignoring generated
    files which don't carry our copyright).
    
    The change in copyright statement is only made to Red Hat statements.
    Change automated with (split over multiple lines for the commit
    message):
    
    find . -name \*.go -exec sed -i -E \
    's/© .... Red Hat, Inc. and others.?/
    SPDX-License-Identifier: Apache-2.0\n\n
    Copyright Contributors to the Submariner project./' {} +
    
    Fixes: #1322
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- test/external/dataplane/connectivity.go
+++ test/external/dataplane/connectivity.go
@@ -1,4 +1,8 @@
 /*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
