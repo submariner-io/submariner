ca6d02e1efe507c209fd2d1eba2b301937daea94
Miguel Angel Ajo <majopela@redhat.com>
2021-05-19 20:25:07 +0200

    Implement multiple public IP resolvers
    The gateway nodes now can be labeled as:
    
    gateway.submariner.io/public-ip=<resolver>[,resolver..]
    where resolvers take the form of <method>:<parameter>
    
    the implemented methods are:
    
    - api
    - lb
    - ipv4
    - dns
    
    this commit also adds additional fallback API public-ip resolvers
    
    In addition to https://api.ipify.org
    - https://api.my-ip.io/ip
    - https://ip4.seeip.org
    
    Signed-off-by: Miguel Angel Ajo <majopela@redhat.com>

--- main.go
+++ main.go
@@ -54,9 +54,7 @@ import (
 	"github.com/submariner-io/submariner/pkg/controllers/tunnel"
 	"github.com/submariner-io/submariner/pkg/endpoint"
 	"github.com/submariner-io/submariner/pkg/natdiscovery"
-	"github.com/submariner-io/submariner/pkg/node"
 	"github.com/submariner-io/submariner/pkg/types"
-	"github.com/submariner-io/submariner/pkg/util"
 )
 
 var (
@@ -112,9 +110,9 @@ func main() {
 		klog.Fatalf("Error creating submariner clientset: %s", err.Error())
 	}
 
-	localNode, err := node.GetLocalNode(cfg)
+	k8sClient, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
-		klog.Fatalf("Error getting information on the local node: %s", err.Error())
+		klog.Fatalf("Error creating Kubernetes clientset: %s", err.Error())
 	}
 
 	klog.Info("Creating the cable engine")
@@ -127,7 +125,7 @@ func main() {
 
 	submSpec.CableDriver = strings.ToLower(submSpec.CableDriver)
 
-	localEndpoint, err := endpoint.GetLocal(submSpec, util.GetLocalIP(), localNode)
+	localEndpoint, err := endpoint.GetLocal(submSpec, k8sClient)
 
 	if err != nil {
 		klog.Fatalf("Error creating local endpoint object from %#v: %v", submSpec, err)
