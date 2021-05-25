9e58379d8c4d734f42ac21f19e56415ab3cfffc4
Tom Pantelis <tompantelis@gmail.com>
2021-05-25 17:28:00 -0400

    Add ClusterGlobalEgressIP controller
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

---
+++ pkg/globalnet/controllers/cluster_egressip_controller.go
@@ -0,0 +1,141 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package controllers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/submariner-io/admiral/pkg/federate"
+	"github.com/submariner-io/admiral/pkg/syncer"
+	"github.com/submariner-io/admiral/pkg/util"
+	submarinerv1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
+	"github.com/submariner-io/submariner/pkg/globalnet/controllers/ipam"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog"
+)
+
+func NewClusterGlobalEgressIPController(config syncer.ResourceSyncerConfig, pool *ipam.IPPool) (Interface, error) {
+	var err error
+
+	klog.Info("Creating ClusterGlobalEgressIP controller")
+
+	defaultEgressIP := &submarinerv1.ClusterGlobalEgressIP{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ClusterGlobalEgressIPName,
+		},
+	}
+
+	defaultEgressIPObj, gvr, err := util.ToUnstructuredResource(defaultEgressIP, config.RestMapper)
+	if err != nil {
+		return nil, err
+	}
+
+	client := config.SourceClient.Resource(*gvr)
+	_, err = client.Get(context.TODO(), ClusterGlobalEgressIPName, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		_, err = client.Create(context.TODO(), defaultEgressIPObj, metav1.CreateOptions{})
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO - reserve the allocated IPs in the IPPool cache.
+
+	controller := &clusterGlobalEgressIPController{
+		pool:   pool,
+		stopCh: make(chan struct{}),
+	}
+
+	controller.resourceSyncer, err = syncer.NewResourceSyncer(&syncer.ResourceSyncerConfig{
+		Name:                "ClusterGlobalEgressIP syncer",
+		ResourceType:        &submarinerv1.ClusterGlobalEgressIP{},
+		SourceClient:        config.SourceClient,
+		SourceNamespace:     corev1.NamespaceAll,
+		RestMapper:          config.RestMapper,
+		Federator:           federate.NewUpdateStatusFederator(config.SourceClient, config.RestMapper, corev1.NamespaceAll),
+		Scheme:              config.Scheme,
+		Transform:           controller.process,
+		ResourcesEquivalent: syncer.AreSpecsEquivalent,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return controller, nil
+}
+
+func (c *clusterGlobalEgressIPController) Start() error {
+	klog.Info("Starting ClusterGlobalEgressIP controller")
+
+	return c.resourceSyncer.Start(c.stopCh)
+}
+
+func (c *clusterGlobalEgressIPController) Stop() {
+	close(c.stopCh)
+}
+
+func (c *clusterGlobalEgressIPController) process(from runtime.Object, numRequeues int, op syncer.Operation) (runtime.Object, bool) {
+	clusterGlobalEgressIP := from.(*submarinerv1.ClusterGlobalEgressIP)
+
+	klog.Infof("Processing %sd %#v", op, clusterGlobalEgressIP)
+
+	switch op {
+	case syncer.Create:
+		prevStatus := clusterGlobalEgressIP.Status
+		requeue := c.onCreate(clusterGlobalEgressIP)
+
+		return checkGlobalEgressIPStatusChanged(&prevStatus, &clusterGlobalEgressIP.Status, clusterGlobalEgressIP), requeue
+	case syncer.Update:
+		// TODO handle update
+	case syncer.Delete:
+		return nil, c.onRemove(clusterGlobalEgressIP)
+	}
+
+	return nil, false
+}
+
+func (c *clusterGlobalEgressIPController) onCreate(egressIP *submarinerv1.ClusterGlobalEgressIP) bool {
+	key, _ := cache.MetaNamespaceKeyFunc(egressIP)
+
+	if egressIP.Name != ClusterGlobalEgressIPName {
+		tryAppendStatusCondition(&egressIP.Status, &metav1.Condition{
+			Type:   string(submarinerv1.GlobalEgressIPAllocated),
+			Status: metav1.ConditionFalse,
+			Reason: "InvalidInstance",
+			Message: fmt.Sprintf("Only the ClusterGlobalEgressIP instance with the well-known name %q is supported",
+				ClusterGlobalEgressIPName),
+		})
+
+		return false
+	}
+
+	return allocateIPs(key, egressIP.Spec.NumberOfIPs, c.pool, &egressIP.Status)
+}
+
+func (c *clusterGlobalEgressIPController) onRemove(egressIP *submarinerv1.ClusterGlobalEgressIP) bool { // nolint unparam
+	// TODO - remove IP table rules for the allocated IPs
+
+	return false
+}
---
+++ pkg/globalnet/controllers/cluster_egressip_controller_test.go
@@ -0,0 +1,118 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package controllers_test
+
+import (
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/submariner-io/admiral/pkg/syncer"
+	submarinerv1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
+	"github.com/submariner-io/submariner/pkg/globalnet/controllers"
+	"github.com/submariner-io/submariner/pkg/globalnet/controllers/ipam"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = Describe("ClusterGlobalEgressIP controller", func() {
+	t := newClusterGlobalEgressIPControllerTestDriver()
+
+	When("the well-known ClusterGlobalEgressIP does not exist on startup", func() {
+		It("should create it and the global IP", func() {
+			t.awaitClusterGlobalEgressIPStatusAllocated(controllers.ClusterGlobalEgressIPName, 1)
+			// TODO - verify IP tables
+		})
+	})
+
+	When("the well-known ClusterGlobalEgressIP exists on startup", func() {
+		var existing *submarinerv1.ClusterGlobalEgressIP
+
+		BeforeEach(func() {
+			existing = newClusterGlobalEgressIP(controllers.ClusterGlobalEgressIPName, 3)
+			existing.Status.AllocatedIPs = []string{"169.254.1.100", "169.254.1.101", "169.254.1.102"}
+			t.createClusterGlobalEgressIP(existing)
+		})
+
+		It("should not reallocate the global IPs", func() {
+			Consistently(func() []string {
+				return getGlobalEgressIPStatus(t.clusterGlobalEgressIPs, existing.Name).AllocatedIPs
+			}, 200*time.Millisecond).Should(Equal(existing.Status.AllocatedIPs))
+		})
+
+		It("should not update the Status conditions", func() {
+			Consistently(func() int {
+				return len(getGlobalEgressIPStatus(t.clusterGlobalEgressIPs, existing.Name).Conditions)
+			}, 200*time.Millisecond).Should(Equal(0))
+		})
+
+		It("should reserve the previously allocated IPs", func() {
+			// TODO - verify
+		})
+	})
+
+	When("a ClusterGlobalEgressIP is created without the well-known name", func() {
+		JustBeforeEach(func() {
+			t.createClusterGlobalEgressIP(newClusterGlobalEgressIP("other name", 1))
+		})
+
+		It("should not allocate the global IP", func() {
+			awaitGlobalEgressIPStatus(t.clusterGlobalEgressIPs, "other name", t.globalCIDR,
+				0, 0, metav1.Condition{
+					Type:   string(submarinerv1.GlobalEgressIPAllocated),
+					Status: metav1.ConditionFalse,
+					Reason: "InvalidInstance",
+				})
+		})
+	})
+})
+
+type clusterGlobalEgressIPControllerTestDriver struct {
+	*testDriverBase
+}
+
+func newClusterGlobalEgressIPControllerTestDriver() *clusterGlobalEgressIPControllerTestDriver {
+	t := &clusterGlobalEgressIPControllerTestDriver{}
+
+	BeforeEach(func() {
+		t.testDriverBase = newTestDriverBase()
+	})
+
+	JustBeforeEach(func() {
+		t.start()
+	})
+
+	AfterEach(func() {
+		t.testDriverBase.afterEach()
+	})
+
+	return t
+}
+
+func (t *clusterGlobalEgressIPControllerTestDriver) start() {
+	pool, err := ipam.NewIPPool(t.globalCIDR)
+	Expect(err).To(Succeed())
+
+	t.controller, err = controllers.NewClusterGlobalEgressIPController(syncer.ResourceSyncerConfig{
+		SourceClient: t.dynClient,
+		RestMapper:   t.restMapper,
+		Scheme:       t.scheme,
+	}, pool)
+
+	Expect(err).To(Succeed())
+	Expect(t.controller.Start()).To(Succeed())
+}
