7b1c96062d8d8758e7cad3fdb68f3ded8c6e7acc
Tom Pantelis <tompantelis@gmail.com>
2021-06-24 12:23:02 -0400

    Add GN headless service tests
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/globalnet/controllers/service_export_controller_test.go
+++ pkg/globalnet/controllers/service_export_controller_test.go
@@ -23,6 +23,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/submariner-io/admiral/pkg/syncer"
+	"github.com/submariner-io/admiral/pkg/syncer/test"
 	submarinerv1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
 	"github.com/submariner-io/submariner/pkg/globalnet/controllers"
 	"github.com/submariner-io/submariner/pkg/ipam"
@@ -31,73 +32,157 @@ import (
 )
 
 var _ = Describe("ServiceExport controller", func() {
+	Describe("Cluster IP Service", testClusterIPService)
+	Describe("Headless Service", testHeadlessService)
+})
+
+func testClusterIPService() {
 	t := newServiceExportControllerTestDriver()
 
-	When("an existing cluster IP Service is exported", func() {
+	var service *corev1.Service
+
+	BeforeEach(func() {
+		service = newClusterIPService()
+		t.createIPTableChain("nat", kubeProxyIPTableChainName)
+	})
+
+	When("an existing Service is exported", func() {
 		BeforeEach(func() {
-			t.createIPTableChain("nat", kubeProxyIPTableChainName)
-			t.createServiceExport(t.createService(newClusterIPService()))
+			t.createServiceExport(t.createService(service))
 		})
 
 		It("should create an appropriate GlobalIngressIP", func() {
-			ingressIP := t.awaitGlobalIngressIP(serviceName)
+			ingressIP := t.awaitGlobalIngressIP(service.Name)
 			Expect(ingressIP.Spec.Target).To(Equal(submarinerv1.ClusterIPService))
 			Expect(ingressIP.Spec.ServiceRef).ToNot(BeNil())
-			Expect(ingressIP.Spec.ServiceRef.Name).To(Equal(serviceName))
+			Expect(ingressIP.Spec.ServiceRef.Name).To(Equal(service.Name))
 		})
 
 		Context("and then unexported", func() {
 			It("should delete the GlobalIngressIP", func() {
-				t.awaitGlobalIngressIP(serviceName)
-				Expect(t.serviceExports.Delete(context.TODO(), serviceName, metav1.DeleteOptions{})).To(Succeed())
-				t.awaitNoGlobalIngressIP(serviceName)
+				t.awaitGlobalIngressIP(service.Name)
+				Expect(t.serviceExports.Delete(context.TODO(), service.Name, metav1.DeleteOptions{})).To(Succeed())
+				t.awaitNoGlobalIngressIP(service.Name)
 			})
 		})
 	})
 
-	When("an existing headless Service is exported", func() {
-		BeforeEach(func() {
-			t.createServiceExport(t.createService(newHeadlessService()))
-		})
-
-		It("should create an appropriate GlobalIngressIP", func() {
-			// TODO
-		})
-	})
-
 	When("a Service is created after being exported", func() {
-		var service *corev1.Service
-
 		BeforeEach(func() {
-			service = newClusterIPService()
 			t.createServiceExport(service)
-			t.createIPTableChain("nat", kubeProxyIPTableChainName)
 		})
 
 		It("should eventually create a GlobalIngressIP", func() {
-			t.awaitNoGlobalIngressIP(serviceName)
+			t.awaitNoGlobalIngressIP(service.Name)
 			t.createService(service)
-			t.awaitGlobalIngressIP(serviceName)
+			t.awaitGlobalIngressIP(service.Name)
 		})
 	})
 
 	When("an unsupported type Service is exported", func() {
 		BeforeEach(func() {
-			t.createServiceExport(t.createService(&corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: serviceName,
-				},
-				Spec: corev1.ServiceSpec{
-					Type: corev1.ServiceTypeNodePort,
-				},
-			}))
+			service.Spec.Type = corev1.ServiceTypeNodePort
+			t.createServiceExport(t.createService(service))
 		})
 
 		It("should not create a GlobalIngressIP", func() {
-			t.awaitNoGlobalIngressIP(serviceName)
+			t.awaitNoGlobalIngressIP(service.Name)
 		})
 	})
-})
+}
+
+func testHeadlessService() {
+	t := newServiceExportControllerTestDriver()
+
+	var service *corev1.Service
+	var backendPod *corev1.Pod
+
+	BeforeEach(func() {
+		service = newHeadlessService()
+		backendPod = newHeadlessServicePod(service.Name)
+		t.createServiceExport(t.createService(service))
+	})
+
+	When("a backend Pod for an exported Service is created", func() {
+		BeforeEach(func() {
+			t.createPod(backendPod)
+		})
+
+		It("should create an appropriate GlobalIngressIP", func() {
+			t.awaitHeadlessGlobalIngressIP(service.Name, backendPod.Name)
+		})
+
+		Context("and then deleted", func() {
+			It("should delete the GlobalIngressIP", func() {
+				ingressIP := t.awaitHeadlessGlobalIngressIP(service.Name, backendPod.Name)
+				t.deletePod(backendPod)
+				t.awaitNoGlobalIngressIP(ingressIP.Name)
+			})
+		})
+	})
+
+	When("a backend Pod for an exported Service isn't initially running", func() {
+		BeforeEach(func() {
+			backendPod.Status.Phase = corev1.PodPending
+			t.createPod(backendPod)
+		})
+
+		It("should eventually create a GlobalIngressIP", func() {
+			t.awaitNoGlobalIngressIPs()
+
+			backendPod.Status.Phase = corev1.PodRunning
+			test.UpdateResource(t.pods.Namespace(namespace), backendPod)
+			t.awaitHeadlessGlobalIngressIP(service.Name, backendPod.Name)
+		})
+	})
+
+	When("a backend Pod for an exported Service doesn't initially have an IP", func() {
+		BeforeEach(func() {
+			backendPod.Status.PodIP = ""
+			t.createPod(backendPod)
+		})
+
+		It("should eventually create a GlobalIngressIP", func() {
+			t.awaitNoGlobalIngressIPs()
+
+			backendPod.Status.PodIP = "154.67.82.2"
+			test.UpdateResource(t.pods.Namespace(namespace), backendPod)
+			t.awaitHeadlessGlobalIngressIP(service.Name, backendPod.Name)
+		})
+	})
+
+	When("a Service is unexported", func() {
+		var backendPod2 *corev1.Pod
+		var otherPod *corev1.Pod
+
+		BeforeEach(func() {
+			t.createPod(backendPod)
+			backendPod2 = t.createPod(newHeadlessServicePod(service.Name))
+			otherPod = t.createPod(newPod(namespace))
+		})
+
+		It("should delete the GlobalIngressIP objects associated to the backend Pods", func() {
+			ingressIP1 := t.awaitHeadlessGlobalIngressIP(service.Name, backendPod.Name)
+			ingressIP2 := t.awaitHeadlessGlobalIngressIP(service.Name, backendPod2.Name)
+
+			Expect(t.serviceExports.Delete(context.TODO(), service.Name, metav1.DeleteOptions{})).To(Succeed())
+			t.awaitNoGlobalIngressIP(ingressIP1.Name)
+			t.awaitNoGlobalIngressIP(ingressIP2.Name)
+
+			test.GetResource(t.pods.Namespace(namespace), otherPod)
+		})
+	})
+
+	When("a Pod not associated to an exported Service is created", func() {
+		BeforeEach(func() {
+			t.createPod(newPod(namespace))
+		})
+
+		It("should not create a GlobalIngressIP", func() {
+			t.awaitNoGlobalIngressIPs()
+		})
+	})
+}
 
 type serviceExportControllerTestDriver struct {
 	*testDriverBase
