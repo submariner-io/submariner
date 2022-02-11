242466388d08788528b07b33568eea048d91f22e
Masaki Kimura <masaki.kimura@hitachivantara.com>
2022-02-11 19:12:07 +0000

    Implement endpoints controller for service without selector
    Signed-off-by: Masaki Kimura <masaki.kimura@hitachivantara.com>

---
+++ pkg/globalnet/controllers/endpoints_controller.go
@@ -0,0 +1,146 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"reflect"
+
+	"github.com/pkg/errors"
+	"github.com/submariner-io/admiral/pkg/federate"
+	"github.com/submariner-io/admiral/pkg/syncer"
+	"github.com/submariner-io/admiral/pkg/util"
+	"github.com/submariner-io/submariner/pkg/globalnet/constants"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/klog"
+)
+
+func startEndpointsController(name, namespace string, config *syncer.ResourceSyncerConfig) (*endpointsController, error) {
+	// We'll panic if config is nil, this is intentional
+	var err error
+
+	klog.Infof("Creating Endpoints controller for %s/%s", namespace, name)
+
+	_, gvr, err := util.ToUnstructuredResource(&corev1.Endpoints{}, config.RestMapper)
+	if err != nil {
+		return nil, errors.Wrap(err, "error converting resource")
+	}
+
+	controller := &endpointsController{
+		baseSyncerController: newBaseSyncerController(),
+		name:                 name,
+		namespace:            namespace,
+		endpoints:            config.SourceClient.Resource(*gvr),
+	}
+
+	fieldSelector := fields.Set(map[string]string{"metadata.name": name}).AsSelector().String()
+
+	controller.resourceSyncer, err = syncer.NewResourceSyncer(&syncer.ResourceSyncerConfig{
+		Name:                "Endpoints syncer",
+		ResourceType:        &corev1.Endpoints{},
+		SourceClient:        config.SourceClient,
+		SourceNamespace:     namespace,
+		RestMapper:          config.RestMapper,
+		Federator:           federate.NewCreateOrUpdateFederator(config.SourceClient, config.RestMapper, namespace, "" /* localClusterID */),
+		Transform:           controller.process,
+		SourceFieldSelector: fieldSelector,
+		ResourcesEquivalent: areEndpointsEqual,
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "error creating the syncer")
+	}
+
+	if err := controller.Start(); err != nil {
+		return nil, err
+	}
+
+	return controller, nil
+}
+
+func (c *endpointsController) Stop() {
+	c.baseController.Stop()
+}
+
+func (c *endpointsController) Start() error {
+	err := c.baseSyncerController.Start()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *endpointsController) process(from runtime.Object, numRequeues int, op syncer.Operation) (runtime.Object, bool) {
+	ep := from.(*corev1.Endpoints)
+
+	switch op {
+	case syncer.Create, syncer.Update:
+		return c.onCreateOrUpdate(ep)
+	case syncer.Delete:
+		return c.onDelete(ep)
+	}
+
+	return nil, false
+}
+
+func (c *endpointsController) onCreateOrUpdate(ep *corev1.Endpoints) (runtime.Object, bool) {
+	key, _ := cache.MetaNamespaceKeyFunc(ep)
+
+	klog.Infof("Processing Endpoints %q", key)
+
+	// Skip cloning already cloned Endpoints resource
+	if _, ok := ep.Labels[constants.EndpointClonedFrom]; ok {
+		klog.Infof("Skip cloning already cloned Endpoints %q", key)
+
+		return ep, false
+	}
+
+	clonedEp := &corev1.Endpoints{}
+	clonedEp.Namespace = ep.Namespace
+	clonedEp.Name = GetInternalSvcName(ep.Name)
+	clonedEp.Labels = map[string]string{constants.EndpointClonedFrom: ep.Name}
+	clonedEp.Subsets = ep.Subsets
+
+	return clonedEp, false
+}
+
+func (c *endpointsController) onDelete(ep *corev1.Endpoints) (runtime.Object, bool) {
+	key, _ := cache.MetaNamespaceKeyFunc(ep)
+
+	klog.Infof("Endpoints %q deleted", key)
+
+	return &corev1.Endpoints{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      GetInternalSvcName(ep.Name),
+			Namespace: ep.Namespace,
+		},
+	}, false
+}
+
+func areEndpointsEqual(obj1, obj2 *unstructured.Unstructured) bool {
+	subsets1, _, _ := unstructured.NestedSlice(obj1.Object, "subsets")
+	subsets2, _, _ := unstructured.NestedSlice(obj2.Object, "subsets")
+
+	return reflect.DeepEqual(subsets1, subsets2)
+}
---
+++ pkg/globalnet/controllers/service_export_endpoints_controllers.go
@@ -0,0 +1,144 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+
+Copyright Contributors to the Submariner project.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/submariner-io/admiral/pkg/syncer"
+	"github.com/submariner-io/admiral/pkg/util"
+	"github.com/submariner-io/submariner/pkg/globalnet/constants"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/klog"
+	mcsv1a1 "sigs.k8s.io/mcs-api/pkg/apis/v1alpha1"
+)
+
+func NewServiceExportEndpointsControllers(config *syncer.ResourceSyncerConfig) (*ServiceExportEndpointsControllers, error) {
+	// We'll panic if config is nil, this is intentional
+	// Ensure that cloned endpoints whose original endpoints are deleted while the controller stops are deleted.
+	_, gvr, err := util.ToUnstructuredResource(&corev1.Endpoints{}, config.RestMapper)
+	if err != nil {
+		return nil, errors.Wrap(err, "error converting resource")
+	}
+
+	client := config.SourceClient.Resource(*gvr)
+
+	err = ensureClonedHasOriginal(client)
+	if err != nil {
+		return nil, errors.Wrap(err, "error ensuring cloned Endpoints has original")
+	}
+
+	return &ServiceExportEndpointsControllers{
+		controllers: map[string]*endpointsController{},
+		config:      *config,
+	}, nil
+}
+
+func (c *ServiceExportEndpointsControllers) start(se *mcsv1a1.ServiceExport) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	key := c.key(se.Name, se.Namespace)
+	if _, exists := c.controllers[key]; exists {
+		return nil
+	}
+
+	controller, err := startEndpointsController(se.Name, se.Namespace, &c.config)
+	if err != nil {
+		return err
+	}
+
+	c.controllers[key] = controller
+
+	return nil
+}
+
+func (c *ServiceExportEndpointsControllers) stopAll() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	for _, controller := range c.controllers {
+		controller.Stop()
+	}
+
+	c.controllers = map[string]*endpointsController{}
+}
+
+func (c *ServiceExportEndpointsControllers) stopAndCleanup(seName, seNamespace string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	key := c.key(seName, seNamespace)
+
+	controller, exists := c.controllers[key]
+	if exists {
+		controller.Stop()
+		delete(c.controllers, key)
+	}
+}
+
+func ensureClonedHasOriginal(client dynamic.NamespaceableResourceInterface) error {
+	list, err := client.Namespace(corev1.NamespaceAll).List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return errors.Wrap(err, "error listing the resources")
+	}
+
+	for _, obj1 := range list.Items {
+		var (
+			found, ok bool
+			origEp    string
+		)
+
+		labels := obj1.GetLabels()
+		if origEp, ok = labels[constants.EndpointClonedFrom]; !ok {
+			// Not a cloned Endpoints.
+			continue
+		}
+
+		for _, obj2 := range list.Items {
+			if obj1.GetNamespace() == obj2.GetNamespace() && origEp == obj2.GetName() {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			// Original Endpoints found.
+			continue
+		}
+
+		// Delete the cloned Endpoints that doesn't have original Endpoints.
+		klog.Infof("Deleting cloned Endpoints %s/%s that doesn't have original Endpoints %s/%s",
+			obj1.GetNamespace(), obj1.GetName(), obj1.GetNamespace(), origEp)
+
+		err := deleteEndpoints(obj1.GetNamespace(), obj1.GetName(), client)
+		if err != nil {
+			return errors.Wrap(err, "error deleting cloned Endpoints without original")
+		}
+	}
+
+	return nil
+}
+
+func (c *ServiceExportEndpointsControllers) key(n, ns string) string {
+	return ns + "/" + n
+}
