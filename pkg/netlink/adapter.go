4def01bbf7dfeed632c572dc465b90628cdee003
Tom Pantelis <tompantelis@gmail.com>
2022-03-18 13:19:38 -0400

    Add AddDestinationRoutes method to netlink Interface
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/netlink/adapter.go
+++ pkg/netlink/adapter.go
@@ -16,10 +16,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// nolint:wrapcheck // Most of the functions are simple wrappers so we'll let the caller wrap errors.
 package netlink
 
 import (
+	"net"
 	"os"
+	"syscall"
 
 	"github.com/pkg/errors"
 	"github.com/vishvananda/netlink"
@@ -33,6 +36,38 @@ func (a *Adapter) RuleAddIfNotPresent(rule *netlink.Rule) error {
 	err := a.RuleAdd(rule)
 	if err != nil && !os.IsExist(err) {
 		return errors.Wrapf(err, "failed to add rule %s", rule)
+	}
+
+	return nil
+}
+
+func (a *Adapter) RouteAddOrReplace(route *netlink.Route) error {
+	err := netlink.RouteAdd(route)
+
+	if errors.Is(err, syscall.EEXIST) {
+		err = netlink.RouteReplace(route)
+	}
+
+	return err
+}
+
+func (a *Adapter) AddDestinationRoutes(destIPs []net.IPNet, gwIP, srcIP net.IP, linkIndex, tableID int) error {
+	for i := range destIPs {
+		route := &netlink.Route{
+			LinkIndex: linkIndex,
+			Src:       srcIP,
+			Dst:       &destIPs[i],
+			Gw:        gwIP,
+			Type:      netlink.NDA_DST,
+			Flags:     netlink.NTF_SELF,
+			Priority:  100,
+			Table:     tableID,
+		}
+
+		err := a.RouteAddOrReplace(route)
+		if err != nil {
+			return errors.Wrapf(err, "unable to add the route entry %#v", route)
+		}
 	}
 
 	return nil
