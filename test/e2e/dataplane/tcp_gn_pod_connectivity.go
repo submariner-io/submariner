d200ecfc1b0ae172f0778c435b0524c1f73ebb22
Sridhar Gaddam <sgaddam@redhat.com>
2022-09-23 18:49:36 +0530

    Validate an e2e test-case in reverse direction (#2033)
    Although not common, in some deployments, we have
    seen that e2e tests pass in one direction but fail
    in the reverse direction. This PR includes an additional
    test-case which swaps the client/server and validates
    nonGW to nonGW use-case.
    
    Signed-off-by: Sridhar Gaddam <sgaddam@redhat.com>
    (cherry picked from commit 60f560fc37b7fe791902cf14f9e538254326007d)

--- test/e2e/dataplane/tcp_gn_pod_connectivity.go
+++ test/e2e/dataplane/tcp_gn_pod_connectivity.go
@@ -30,6 +30,8 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 	var toEndpointType tcp.EndpointType
 	var networking framework.NetworkingType
 	var egressIPType subFramework.GlobalEgressIPType
+	var fromCluster framework.ClusterIndex
+	var toCluster framework.ClusterIndex
 
 	verifyInteraction := func(fromClusterScheduling, toClusterScheduling framework.NetworkPodScheduling) {
 		It("should have sent the expected data from the pod to the other pod", func() {
@@ -47,9 +49,9 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 				Framework:             f,
 				ToEndpointType:        toEndpointType,
 				Networking:            networking,
-				FromCluster:           framework.ClusterA,
+				FromCluster:           fromCluster,
 				FromClusterScheduling: fromClusterScheduling,
-				ToCluster:             framework.ClusterB,
+				ToCluster:             toCluster,
 				ToClusterScheduling:   toClusterScheduling,
 			}, subFramework.GetGlobalnetEgressParams(egressIPType))
 		})
@@ -60,6 +62,8 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 			toEndpointType = tcp.GlobalServiceIP
 			networking = framework.PodNetworking
 			egressIPType = subFramework.ClusterSelector
+			fromCluster = framework.ClusterA
+			toCluster = framework.ClusterB
 		})
 
 		When("the pod is not on a gateway and the remote service is not on a gateway", func() {
@@ -80,6 +84,8 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 			toEndpointType = tcp.GlobalServiceIP
 			networking = framework.PodNetworking
 			egressIPType = subFramework.NameSpaceSelector
+			fromCluster = framework.ClusterA
+			toCluster = framework.ClusterB
 		})
 
 		When("the pod is not on a gateway and the remote service is not on a gateway", func() {
@@ -96,6 +102,8 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 			toEndpointType = tcp.GlobalServiceIP
 			networking = framework.PodNetworking
 			egressIPType = subFramework.PodSelector
+			fromCluster = framework.ClusterA
+			toCluster = framework.ClusterB
 		})
 
 		When("the pod is not on a gateway and the remote service is not on a gateway", func() {
@@ -112,6 +120,8 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 			toEndpointType = tcp.GlobalServiceIP
 			networking = framework.HostNetworking
 			egressIPType = subFramework.ClusterSelector
+			fromCluster = framework.ClusterA
+			toCluster = framework.ClusterB
 		})
 
 		When("the pod is not on a gateway and the remote service is not on a gateway", func() {
@@ -128,6 +138,8 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 			toEndpointType = tcp.GlobalPodIP
 			networking = framework.PodNetworking
 			egressIPType = subFramework.ClusterSelector
+			fromCluster = framework.ClusterA
+			toCluster = framework.ClusterB
 		})
 
 		When("the pod is not on a gateway and the remote service is not on a gateway", func() {
@@ -136,6 +148,20 @@ var _ = Describe("[dataplane-globalnet] Basic TCP connectivity tests across over
 
 		When("the pod is on a gateway and the remote service is on a gateway", func() {
 			verifyInteraction(framework.GatewayNode, framework.GatewayNode)
+		})
+	})
+
+	When("a pod connects via TCP to the globalIP of a remote service in reverse direction", func() {
+		BeforeEach(func() {
+			toEndpointType = tcp.GlobalServiceIP
+			networking = framework.PodNetworking
+			egressIPType = subFramework.ClusterSelector
+			fromCluster = framework.ClusterB
+			toCluster = framework.ClusterA
+		})
+
+		When("the pod is not on a gateway and the remote service is not on a gateway", func() {
+			verifyInteraction(framework.NonGatewayNode, framework.NonGatewayNode)
 		})
 	})
 })
