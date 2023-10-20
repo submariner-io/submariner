51f22464fc227be24d1641d45461f43689705204
Tom Pantelis <tompantelis@gmail.com>
2023-10-20 13:43:03 -0400

    Restart leader election on lease renewal failure in gateway
    ...rather than exiting.
    
    Related to https://github.com/submariner-io/submariner/issues/2747
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/gateway/gateway_suite_test.go
+++ pkg/gateway/gateway_suite_test.go
@@ -20,11 +20,16 @@ package gateway_test
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/submariner-io/admiral/pkg/log/kzerolog"
 	submarinerv1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
+	"github.com/submariner-io/submariner/pkg/cable"
+	"github.com/submariner-io/submariner/pkg/cable/fake"
+	"github.com/submariner-io/submariner/pkg/cableengine/syncer"
+	"github.com/submariner-io/submariner/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
@@ -32,9 +37,17 @@ func init() {
 	kzerolog.AddFlags(nil)
 }
 
+var fakeDriver *fake.Driver
+
 var _ = BeforeSuite(func() {
 	kzerolog.InitK8sLogging()
 	Expect(submarinerv1.AddToScheme(scheme.Scheme)).To(Succeed())
+
+	cable.AddDriver(fake.DriverName, func(_ *types.SubmarinerEndpoint, _ *types.SubmarinerCluster) (cable.Driver, error) {
+		return fakeDriver, nil
+	})
+
+	syncer.GatewayUpdateInterval = 50 * time.Millisecond
 })
 
 func TestGateway(t *testing.T) {
