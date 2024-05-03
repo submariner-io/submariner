d48f465ad99ab36555d7948a3927ea0a653db6ce
Stephen Kitt <skitt@redhat.com>
2024-05-03 13:06:27 +0200

    Switch to math/rand/v2
    This provides more robust functions and defaults than math/rand, and
    provides sensible global convenience functions so a package-specific
    source is no longer necessary. See https://go.dev/blog/randv2 for
    context.
    
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- pkg/endpoint/public_ip.go
+++ pkg/endpoint/public_ip.go
@@ -22,7 +22,7 @@ import (
 	"bytes"
 	"context"
 	"io"
-	"math/rand"
+	"math/rand/v2"
 	"net"
 	"net/http"
 	"regexp"
@@ -51,9 +51,6 @@ var publicIPMethods = map[string]publicIPResolverFunction{
 
 var IPv4RE = regexp.MustCompile(`(?:\d{1,3}\.){3}\d{1,3}`)
 
-//nolint:gosec // This is only used to shuffle the list of resolvers
-var randgen = rand.New(rand.NewSource(time.Now().UnixNano()))
-
 func getPublicIPResolvers() string {
 	serverList := []string{
 		"api:ip4.seeip.org", "api:ipecho.net/plain", "api:ifconfig.me",
@@ -61,7 +58,7 @@ func getPublicIPResolvers() string {
 		"api:myexternalip.com/raw", "api:4.tnedi.me", "api:api.ipify.org",
 	}
 
-	randgen.Shuffle(len(serverList), func(i, j int) { serverList[i], serverList[j] = serverList[j], serverList[i] })
+	rand.Shuffle(len(serverList), func(i, j int) { serverList[i], serverList[j] = serverList[j], serverList[i] })
 
 	return strings.Join(serverList, ",")
 }
