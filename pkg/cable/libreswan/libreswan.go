598a6106845054e0bdf76a5dd5052d58847c713b
Tom Pantelis <tompantelis@gmail.com>
2024-07-15 08:59:47 -0400

    Remove metrics.RecordNoConnections
    This function is only called by libreswan.refreshConnectionStatus
    to delete all current metrics but really isn't necessary
    (nor desired) since the metrics are recorded from the stored
    connections so they're immediately re-added.
    
    Removing RecordNoConnections also eliminates a TODO which would
    become relevant if multiple concurrent cable drivers are ever
    supported.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/cable/libreswan/libreswan.go
+++ pkg/cable/libreswan/libreswan.go
@@ -243,8 +243,6 @@ func (i *libreswan) refreshConnectionStatus() error {
 		return err
 	}
 
-	cable.RecordNoConnections()
-
 	localSubnets := extractSubnets(&i.localEndpoint.Spec)
 
 	for j := range i.connections {
