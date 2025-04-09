65837ade7b58f09da8b691cf153a8c841bac7934
Tom Pantelis <tompantelis@gmail.com>
2025-04-09 13:39:59 -0400

    Refactor and adjust public IP resolver unit tests
    Each resolver method is split into its own function and code reused
    between IPv4 and IPv6. Also added more cases to improve coverage.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- pkg/endpoint/public_ip_internal_test.go
+++ pkg/endpoint/public_ip_internal_test.go
@@ -19,11 +19,17 @@ limitations under the License.
 package endpoint
 
 import (
+	"errors"
+	"fmt"
 	"net"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	submarinerv1 "github.com/submariner-io/submariner/pkg/apis/submariner.io/v1"
 	"github.com/submariner-io/submariner/pkg/types"
 	v1 "k8s.io/api/core/v1"
 	v1meta "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -73,232 +79,299 @@ var _ = Describe("firstIPInString", func() {
 const (
 	testServiceName = "my-loadbalancer"
 	testNamespace   = "namespace"
+	testIPv4        = "1.2.3.4"
+	testIPv6        = "2001:4860:4860::8765"
+	testIPv4DNS     = "4.3.2.1"
+	testIPv6DNS     = "2001:db8::1234"
+	dnsHostv4       = testIPv4DNS + ".nip.io"
+	dnsHostv6       = testIPv6DNS + ".sslip.io"
 )
 
-var _ = Describe("public ip resolvers", func() {
-	var submSpec *types.SubmarinerSpecification
-	var backendConfig map[string]string
+var _ = Describe("Public IP resolvers", func() {
+	Describe("IP Family", testIPFamilyResolver)
+	Describe("DNS", testDNSResolver)
+	Describe("API", testAPIResolver)
+	Describe("LoadBalancer", testLoadBalancerResolver)
+	Describe("Air gapped", testResolverInAirGapped)
+	Describe("Multiple", testMultipleResolvers)
+})
 
-	const (
-		publicIPConfig   = "public-ip"
-		testIPDNS        = "4.3.2.1"
-		testIP           = "1.2.3.4"
-		testIPv6         = "2001:4860:4860::8765"
-		testIPv6DNS      = "2001:db8::1234"
-		testIPv6DNSSslip = "2001-db8--1234"
-		dnsHost          = testIPDNS + ".nip.io"
-		dnsHostv6        = testIPv6DNSSslip + ".sslip.io"
-		ipv4PublicIP     = "ipv4:" + testIP
-		ipv6PublicIP     = "ipv6=" + testIPv6
-		lbPublicIP       = "lb:" + testServiceName
-	)
+func testIPFamilyResolver() {
+	t := newResolverTestDriver()
+
+	testGetPublicIP := func(family k8snet.IPFamily, prefix, expectedIP string) {
+		When(fmt.Sprintf("an IPv%s entry is specified", family), func() {
+			It("should return the IP address", func() {
+				backendConfig := map[string]string{submarinerv1.PublicIP: prefix + ":" + expectedIP}
+				ip, resolver, err := getPublicIP(family, t.submSpec, fake.NewClientset(), backendConfig, false)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip).To(Equal(expectedIP))
+				Expect(resolver).To(Equal(backendConfig[submarinerv1.PublicIP]))
+			})
+		})
+	}
+
+	testGetPublicIP(k8snet.IPv4, submarinerv1.IPv4, testIPv4)
+	testGetPublicIP(k8snet.IPv6, submarinerv1.IPv6, testIPv6)
+}
+
+func testDNSResolver() {
+	t := newResolverTestDriver()
+
+	testGetPublicIP := func(family k8snet.IPFamily, hostname, expectedIP string) {
+		When(fmt.Sprintf("an IPv%s host name is specified", family), func() {
+			It("should return the resolved IP address", func() {
+				backendConfig := map[string]string{submarinerv1.PublicIP: submarinerv1.DNS + ":" + hostname}
+				ip, resolver, err := getPublicIP(family, t.submSpec, fake.NewClientset(), backendConfig, false)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip).To(Equal(expectedIP))
+				Expect(resolver).To(Equal(backendConfig[submarinerv1.PublicIP]))
+			})
+		})
+	}
+
+	testGetPublicIP(k8snet.IPv4, dnsHostv4, testIPv4DNS)
+	testGetPublicIP(k8snet.IPv6, dnsHostv6, testIPv6DNS)
+}
+
+func testAPIResolver() {
+	t := newResolverTestDriver()
 
 	BeforeEach(func() {
-		submSpec = &types.SubmarinerSpecification{
+		t.setupHTTPServer(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "v4 %s v6 %s", testIPv4DNS, testIPv6DNS)
+		})
+	})
+
+	testGetPublicIP := func(family k8snet.IPFamily, expectedIP string) {
+		Context(fmt.Sprintf("with IPv%s requested", family), func() {
+			It(fmt.Sprintf("should return a valid IPv%s address", family), func() {
+				backendConfig := map[string]string{submarinerv1.PublicIP: submarinerv1.API + ":" + t.httpServerURL}
+				ip, resolver, err := getPublicIP(family, t.submSpec, fake.NewClientset(), backendConfig, false)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip).To(Equal(expectedIP))
+				Expect(resolver).To(Equal(backendConfig[submarinerv1.PublicIP]))
+			})
+		})
+	}
+
+	testGetPublicIP(k8snet.IPv4, testIPv4DNS)
+	testGetPublicIP(k8snet.IPv6, testIPv6DNS)
+}
+
+func testLoadBalancerResolver() {
+	const lbPublicIP = submarinerv1.LoadBalancer + ":" + testServiceName
+
+	t := newResolverTestDriver()
+
+	BeforeEach(func() {
+		loadBalancerRetryConfig.Cap = 1 * time.Second
+		loadBalancerRetryConfig.Duration = 50 * time.Millisecond
+		loadBalancerRetryConfig.Steps = 1
+	})
+
+	v4IngressWithIP := v1.LoadBalancerIngress{IP: testIPv4}
+	v6IngressWithIP := v1.LoadBalancerIngress{IP: testIPv6}
+	ipIngressses := map[k8snet.IPFamily]v1.LoadBalancerIngress{k8snet.IPv4: v4IngressWithIP, k8snet.IPv6: v6IngressWithIP}
+
+	v4IngressWithHostname := v1.LoadBalancerIngress{Hostname: dnsHostv4}
+	v6IngressithHostname := v1.LoadBalancerIngress{Hostname: dnsHostv6}
+	dnsIPs := map[k8snet.IPFamily]string{k8snet.IPv4: testIPv4DNS, k8snet.IPv6: testIPv6DNS}
+
+	invokeGetPublicIP := func(family k8snet.IPFamily, ingresses ...v1.LoadBalancerIngress) (string, string, error) {
+		ip, resolver, err := getPublicIP(family, t.submSpec, fake.NewClientset(&v1.Service{
+			ObjectMeta: v1meta.ObjectMeta{
+				Name:      testServiceName,
+				Namespace: testNamespace,
+			},
+			Status: v1.ServiceStatus{
+				LoadBalancer: v1.LoadBalancerStatus{
+					Ingress: ingresses,
+				},
+			},
+		}), map[string]string{submarinerv1.PublicIP: lbPublicIP}, false)
+
+		return ip, resolver, err
+	}
+
+	testGetPublicIP := func(family k8snet.IPFamily, expectedIP string, ingresses ...v1.LoadBalancerIngress) {
+		ip, resolver, err := invokeGetPublicIP(family, ingresses...)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(ip).To(Equal(expectedIP))
+		Expect(resolver).To(Equal(lbPublicIP))
+	}
+
+	for _, family := range []k8snet.IPFamily{k8snet.IPv4, k8snet.IPv6} {
+		When(fmt.Sprintf("an IPv%s Ingress IP is configured", family), func() {
+			It("should return the IP address", func() {
+				testGetPublicIP(family, ipIngressses[family].IP, v4IngressWithIP, v6IngressWithIP)
+			})
+		})
+
+		When(fmt.Sprintf("an IPv%s Ingress Hostname is configured", family), func() {
+			It("should return the resolved IP address", func() {
+				testGetPublicIP(family, dnsIPs[family], v4IngressWithHostname, v6IngressithHostname)
+			})
+		})
+	}
+
+	When("no Ingress is configured", func() {
+		It("should return an error", func() {
+			_, _, err := invokeGetPublicIP(k8snet.IPv4)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	When("the Ingress Hostname lookup fails", func() {
+		BeforeEach(func() {
+			LookupIP = func(_ string) ([]net.IP, error) {
+				return nil, errors.New("mock error")
+			}
+		})
+
+		It("should return an error", func() {
+			_, _, err := invokeGetPublicIP(k8snet.IPv4, v4IngressWithHostname)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+
+	When("no Ingress resolves", func() {
+		BeforeEach(func() {
+			LookupIP = func(_ string) ([]net.IP, error) {
+				return []net.IP{}, nil
+			}
+		})
+
+		It("should return an error", func() {
+			_, _, err := invokeGetPublicIP(k8snet.IPv4, v4IngressWithHostname)
+			Expect(err).To(HaveOccurred())
+		})
+	})
+}
+
+func testResolverInAirGapped() {
+	t := newResolverTestDriver()
+
+	testGetPublicIP := func(family k8snet.IPFamily, expectedIP string) {
+		Context(fmt.Sprintf("with IPv%s requested", family), func() {
+			It(fmt.Sprintf("should return a valid IPv%s address", family), func() {
+				backendConfig := map[string]string{
+					submarinerv1.PublicIP: fmt.Sprintf("%s:bogus,%s:%s,%s:%s", submarinerv1.API, submarinerv1.IPv4, testIPv4,
+						submarinerv1.IPv6, testIPv6),
+				}
+				ip, _, err := getPublicIP(family, t.submSpec, fake.NewClientset(), backendConfig, true)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ip).To(Equal(expectedIP))
+			})
+		})
+	}
+
+	testGetPublicIP(k8snet.IPv4, testIPv4)
+	testGetPublicIP(k8snet.IPv6, testIPv6)
+
+	When("no resolver succeeds", func() {
+		It("should return an empty IP", func() {
+			ip, _, err := getPublicIP(k8snet.IPv4, t.submSpec, fake.NewClientset(), map[string]string{
+				submarinerv1.PublicIP: submarinerv1.IPv4 + ":",
+			}, true)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ip).To(BeEmpty())
+		})
+	})
+
+	When("theres no IP family resolver", func() {
+		It("should return an empty IP", func() {
+			ip, _, err := getPublicIP(k8snet.IPv4, t.submSpec, fake.NewClientset(), map[string]string{
+				submarinerv1.PublicIP: submarinerv1.API + ":bogus",
+			}, true)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ip).To(BeEmpty())
+		})
+	})
+}
+
+func testMultipleResolvers() {
+	t := newResolverTestDriver()
+
+	var resolvers []string
+
+	BeforeEach(func() {
+		LookupIP = func(_ string) ([]net.IP, error) {
+			return nil, errors.New("unknown host")
+		}
+
+		t.setupHTTPServer(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(http.StatusNotFound)
+		})
+
+		resolvers = []string{
+			submarinerv1.DNS + ":foo",
+			submarinerv1.API + ":" + t.httpServerURL,
+			submarinerv1.IPv6 + ":" + testIPv6,
+			"unknown:unknown",
+		}
+	})
+
+	It("should return the first successful one", func() {
+		working := submarinerv1.IPv4 + ":1.2.3.4"
+		backendConfig := map[string]string{submarinerv1.PublicIP: strings.Join(append(resolvers, working), ",")}
+		_, resolver, err := getPublicIP(k8snet.IPv4, t.submSpec, fake.NewClientset(), backendConfig, false)
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(resolver).To(Equal(working))
+	})
+
+	When("no resolver succeeds", func() {
+		It("should return an error", func() {
+			backendConfig := map[string]string{submarinerv1.PublicIP: strings.Join(resolvers, ",")}
+			_, _, err := getPublicIP(k8snet.IPv4, t.submSpec, fake.NewClientset(), backendConfig, false)
+
+			Expect(err).To(HaveOccurred())
+		})
+	})
+}
+
+type resolverTestDriver struct {
+	submSpec      *types.SubmarinerSpecification
+	httpServerURL string
+}
+
+func newResolverTestDriver() *resolverTestDriver {
+	t := &resolverTestDriver{}
+
+	BeforeEach(func() {
+		t.submSpec = &types.SubmarinerSpecification{
 			Namespace: testNamespace,
 		}
 
-		backendConfig = map[string]string{}
+		LookupIP = func(host string) ([]net.IP, error) {
+			if host == dnsHostv4 {
+				return []net.IP{net.ParseIP(testIPv4DNS)}, nil
+			} else if host == dnsHostv6 {
+				return []net.IP{net.ParseIP(testIPv6DNS)}, nil
+			}
+
+			return nil, errors.New("unknown host")
+		}
 	})
 
-	When("a LoadBalancer with Ingress IP is specified", func() {
-		It("should return the IPv4", func() {
-			backendConfig[publicIPConfig] = lbPublicIP
-			client := fake.NewClientset(serviceWithIngress(v1.LoadBalancerIngress{Hostname: "", IP: testIP}))
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIP))
-			Expect(resolver).To(Equal(lbPublicIP))
-		})
-		It("should return the IPv6", func() {
-			backendConfig[publicIPConfig] = lbPublicIP
-			client := fake.NewClientset(serviceWithIngress(v1.LoadBalancerIngress{Hostname: "", IP: testIPv6}))
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6))
-			Expect(resolver).To(Equal(lbPublicIP))
-		})
+	return t
+}
+
+func (t *resolverTestDriver) setupHTTPServer(handler http.HandlerFunc) {
+	server := httptest.NewServer(handler)
+
+	DeferCleanup(func() {
+		server.Close()
 	})
 
-	When("a LoadBalancer with Ingress hostname is specified", func() {
-		It("should return the IPv4 address", func() {
-			backendConfig[publicIPConfig] = lbPublicIP
-			client := fake.NewClientset(serviceWithIngress(v1.LoadBalancerIngress{
-				Hostname: dnsHost,
-				IP:       "",
-			}))
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPDNS))
-			Expect(resolver).To(Equal(lbPublicIP))
-		})
-		It("should return the IPv6 address", func() {
-			backendConfig[publicIPConfig] = lbPublicIP
-			client := fake.NewClientset(serviceWithIngress(v1.LoadBalancerIngress{
-				Hostname: dnsHostv6,
-				IP:       "",
-			}))
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6DNS))
-			Expect(resolver).To(Equal(lbPublicIP))
-		})
-	})
-
-	When("a LoadBalancer with no ingress is specified", func() {
-		It("should return error with IPv4", func() {
-			loadBalancerRetryConfig.Cap = 1 * time.Second
-			loadBalancerRetryConfig.Duration = 50 * time.Millisecond
-			loadBalancerRetryConfig.Steps = 1
-			backendConfig[publicIPConfig] = lbPublicIP
-			client := fake.NewClientset(serviceWithIngress())
-			_, _, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).To(HaveOccurred())
-		})
-		It("should return error with IPv6", func() {
-			loadBalancerRetryConfig.Cap = 1 * time.Second
-			loadBalancerRetryConfig.Duration = 50 * time.Millisecond
-			loadBalancerRetryConfig.Steps = 1
-			backendConfig[publicIPConfig] = lbPublicIP
-			client := fake.NewClientset(serviceWithIngress())
-			_, _, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).To(HaveOccurred())
-		})
-	})
-
-	When("an IP entry specified", func() {
-		It("should return the IPv4 address", func() {
-			backendConfig[publicIPConfig] = ipv4PublicIP
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIP))
-			Expect(resolver).To(Equal(ipv4PublicIP))
-		})
-
-		It("should return the IPv6 address", func() {
-			backendConfig[publicIPConfig] = ipv6PublicIP
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6))
-			Expect(resolver).To(Equal(ipv6PublicIP))
-		})
-	})
-
-	When("an IP entry specified in air-gapped deployment", func() {
-		It("should return the IPv4 and not an empty value", func() {
-			backendConfig[publicIPConfig] = ipv4PublicIP
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, true)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIP))
-			Expect(resolver).To(Equal(ipv4PublicIP))
-		})
-		It("should return the IPv6 and not an empty value", func() {
-			backendConfig[publicIPConfig] = ipv6PublicIP
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, true)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6))
-			Expect(resolver).To(Equal(ipv6PublicIP))
-		})
-	})
-
-	When("a DNS entry specified", func() {
-		It("should return the IPv4 address", func() {
-			backendConfig[publicIPConfig] = "dns:" + dnsHost
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPDNS))
-			Expect(resolver).To(Equal(backendConfig[publicIPConfig]))
-		})
-		It("should return the IPv6 address", func() {
-			backendConfig[publicIPConfig] = "dns:" + dnsHostv6
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6DNS))
-			Expect(resolver).To(Equal(backendConfig[publicIPConfig]))
-		})
-	})
-
-	When("an API entry specified", func() {
-		It("should return some IPv4", func() {
-			backendConfig[publicIPConfig] = "api:4.icanhazip.com/"
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			parseIP := net.ParseIP(ip)
-			Expect(parseIP).NotTo(BeNil())
-			Expect(parseIP.To4()).NotTo(BeNil())
-			Expect(resolver).To(Equal(backendConfig[publicIPConfig]))
-		})
-		// temporary disable this test because of GHA IPv6 limitation
-		/*
-			It("should return some IPv6", func() {
-				backendConfig[publicIPConfig] = "api:api64.ipify.org/"
-				client := fake.NewClientset()
-				ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-				Expect(err).ToNot(HaveOccurred())
-				parseIP := net.ParseIP(ip)
-				Expect(parseIP).NotTo(BeNil())
-				Expect(parseIP.To16()).NotTo(BeNil())
-				Expect(resolver).To(Equal(backendConfig[publicIPConfig]))
-			})
-		*/
-	})
-
-	When("multiple entries are specified", func() {
-		It("should return the first IPv4 working one", func() {
-			backendConfig[publicIPConfig] = ipv4PublicIP + ",dns:" + dnsHost
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIP))
-			Expect(resolver).To(Equal(ipv4PublicIP))
-		})
-		It("should return the first IPv6 working one", func() {
-			backendConfig[publicIPConfig] = ipv6PublicIP + ",dns:" + dnsHostv6
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6))
-			Expect(resolver).To(Equal(ipv6PublicIP))
-		})
-	})
-
-	When("multiple entries are specified and the first one doesn't succeed", func() {
-		It("should return the first IPv4 working one", func() {
-			backendConfig[publicIPConfig] = "dns:thisdomaindoesntexistforsure.badbadbad," + ipv4PublicIP
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv4, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIP))
-			Expect(resolver).To(Equal(ipv4PublicIP))
-		})
-		It("should return the first IPv4 working one", func() {
-			backendConfig[publicIPConfig] = "dns:thisdomaindoesntexistforsure.badbadbad," + ipv6PublicIP
-			client := fake.NewClientset()
-			ip, resolver, err := getPublicIP(k8snet.IPv6, submSpec, client, backendConfig, false)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(ip).To(Equal(testIPv6))
-			Expect(resolver).To(Equal(ipv6PublicIP))
-		})
-	})
-})
-
-func serviceWithIngress(ingress ...v1.LoadBalancerIngress) *v1.Service {
-	return &v1.Service{
-		ObjectMeta: v1meta.ObjectMeta{
-			Name:      testServiceName,
-			Namespace: testNamespace,
-		},
-		Status: v1.ServiceStatus{
-			LoadBalancer: v1.LoadBalancerStatus{
-				Ingress: ingress,
-			},
-		},
-	}
+	t.httpServerURL = server.URL
 }
