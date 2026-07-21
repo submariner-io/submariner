e0d6d82cc61efe960f888290a19aa6aeb6f0c56d
sanek9 <sanya0996@gmail.com>
2026-07-22 00:40:44 +0200

    Fix Globalnet deleting headless GIPs across namespaces
    Reconcile GlobalIngressIPs only within the Service namespace so
    exporting the same headless name elsewhere does not drop peers.
    
    Signed-off-by: sanek9 <sanya0996@gmail.com>

--- pkg/globalnet/controllers/ingress_pod_controller.go
+++ pkg/globalnet/controllers/ingress_pod_controller.go
@@ -85,8 +85,11 @@ func startIngressPodController(ctx context.Context, svc *corev1.Service,
 		ServiceRefLabel: svc.Name,
 	}
 
+	// Reconcile only GlobalIngressIPs in this Service's namespace. Listing across
+	// NamespaceAll with just the service name label incorrectly deletes GIPs for
+	// other namespaces that export a headless Service with the same name.
 	ingressIPSelector := labels.Set(selector).AsSelector().String()
-	ingressIPs := config.SourceClient.Resource(*gvr).Namespace(corev1.NamespaceAll)
+	ingressIPs := config.SourceClient.Resource(*gvr).Namespace(svc.Namespace)
 	controller.reconcile(ctx, ingressIPs, ingressIPSelector, "" /* fieldSelector*/, func(obj *unstructured.Unstructured) runtime.Object {
 		podName, exists, _ := unstructured.NestedString(obj.Object, "spec", "podRef", "name")
 		if exists {
